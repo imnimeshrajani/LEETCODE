4fdb3b8213ba1d3faed481eb13178609f1a4f081
imnimeshrajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-08-09 09:23:12 +0530

    Time: 5 ms (86.34%) | Memory: 40.4 MB (99.53%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList<>();
+        if(nums.length==0)return list;
+        int index=0;
+        for(int i=0;i<nums.length-1;i++){
+
+            if(nums[i+1]-nums[i]!=1){
+                if(index!=i){
+                    list.add(nums[index]+"->"+nums[i]);
+                } else list.add(""+nums[index]);
+                index=i+1;
+            }
+        }
+        if(index==nums.length-1){
+            list.add(""+nums[index]);
+        }
+        else{
+            {list.add(nums[index]+"->"+nums[nums.length-1]);}
+        }
+        return list;
+    }
+}
