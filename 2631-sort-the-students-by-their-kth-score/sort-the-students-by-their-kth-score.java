49165f2e724d44fc5af1a25e972a6793cf7125c5
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-04 10:24:53 +0530

    Time: 3 ms (43.57%) | Memory: 49.8 MB (54.32%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
         for(int i = 0; i<score.length;i++){
-            for(int j = i; j<score.length;j++){
+            for(int j = i+1; j<score.length;j++){
                 if(score[i][k]<score[j][k]){
                     int[] temp = score[i];
                     score[i] = score[j];
