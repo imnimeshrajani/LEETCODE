7fe68e8b1e9928ec6c78f2b3db877bdc4bc2f301
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-08 11:55:26 +0530

    Time: 1 ms (95.30%) | Memory: 39.9 MB (39.02%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        if(sx == fx && sy == fy && t == 1) return false;
-        if(Math.abs(sx - fx) > t  || Math.abs(sy - fy) > t) return false;
+        if((sx == fx && sy == fy && t == 1) || (Math.abs(sx - fx) > t  || Math.abs(sy - fy) > t)) return false;
         return true;
     }
 }
