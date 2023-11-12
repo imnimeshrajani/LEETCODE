30d0b395711e89116a8f3adc830f9772c797fb37
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-12 13:42:24 +0530

    Time: 1 ms (97.74%) | Memory: 40 MB (16.19%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,7 +1,6 @@
 class Solution {
     public int reverse(int x) {
         long ans = 0;
-        if(x >= 2147483647 || x <= -2147483648) return 0;
         while(x!=0){
             if(ans!=0) ans *=10;
             ans += x % 10;
