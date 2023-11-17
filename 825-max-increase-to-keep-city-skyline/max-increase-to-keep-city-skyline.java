aaddfee5c6a306d0bf275c642f9fd1ea865bad74
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-17 11:46:16 +0530

    Time: 1 ms (48.33%) | Memory: 43.9 MB (6.11%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int ans = 0;
-
+        int[] cMax = new int[grid.length];
+        int[] rMax = new int[grid[0].length];
+        int cmIndex = 0, rmIndex = 0, ans = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                int rMax = Integer.MIN_VALUE, cMax = Integer.MIN_VALUE;
-                for(int k = 0; k < grid[j].length; k++){
-                    rMax = Math.max(rMax, grid[i][k]);
-                }
-                for(int k = 0; k < grid.length; k++){
-                    cMax = Math.max(cMax, grid[k][j]);
-                }
-                ans += Math.min(rMax,cMax) - grid[i][j];
+                rMax[i] = Math.max(rMax[i], grid[i][j]);
+                cMax[j] = Math.max(cMax[j], grid[i][j]);
+            }
+        }
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                ans += Math.abs(Math.min(rMax[i], cMax[j]) - grid[i][j]);
             }
         }
         return ans;
