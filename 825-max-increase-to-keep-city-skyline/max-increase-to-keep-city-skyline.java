c67bae55788253c45048854ba1da1e0cca4d0000
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-17 11:21:57 +0530

    Time: 3 ms (10.00%) | Memory: 43.6 MB (15.93%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
         int ans = 0;
+
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 int rMax = Integer.MIN_VALUE, cMax = Integer.MIN_VALUE;
@@ -10,7 +11,7 @@ class Solution {
                 for(int k = 0; k < grid.length; k++){
                     cMax = Math.max(cMax, grid[k][j]);
                 }
-                ans += Math.abs(Math.min(rMax,cMax) - grid[i][j]);
+                ans += Math.min(rMax,cMax) - grid[i][j];
             }
         }
         return ans;
