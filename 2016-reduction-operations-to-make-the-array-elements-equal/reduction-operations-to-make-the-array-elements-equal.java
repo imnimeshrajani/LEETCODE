84a97b8d2d0469407a3aced74e5d7191716e2fec
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-19 11:00:46 +0530

    Time: 7 ms (100.00%) | Memory: 54.3 MB (74.34%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -1,16 +1,12 @@
 class Solution {
     public int reductionOperations(int[] nums) {
-        int unique = 0, n = nums.length;
-        if(n == 1) return 0;
-        Arrays.sort(nums);
-        for(int i = 0; i < n - 1; i++) if(nums[i]!=nums[i+1]) unique++;
-        unique++;
-        int count = 1, ans = 0;
-        for(int i = n-1; i > 0 ; i--) {
-            if(nums[i]==nums[i-1]) count++;
-            else {
-                ans += --unique * count;
-                count = 1;
+        int[] freq = new int[50001];
+        for(int i : nums) freq[i]++;
+        int ans = 0, count = 0;
+        for(int i = freq.length-1; i >= 0; i--) {
+            if(freq[i]>0){
+                count += freq[i];
+                ans += count - freq[i]; 
             }
         }
         return ans;
