b682d9e82d375a0494df3f01e4769ec571049767
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-20 11:33:00 +0530

    Time: 32 ms (44.51%) | Memory: 61.4 MB (57.58%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,16 +1,15 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
         int M = 0, P = 0, G = 0;
-        for (String s : garbage) {
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == 'M') M++;
-                else if (s.charAt(j) == 'P') P++;
+        for (int i = 0; i < garbage.length; i++) {
+            for (int j = 0; j < garbage[i].length(); j++) {
+                if (garbage[i].charAt(j) == 'M') M++;
+                else if (garbage[i].charAt(j) == 'P') P++;
                 else G++;
             }
+            if(i!=0 && i < travel.length) travel[i] += travel[i-1];
         }
-        for (int i = 1; i < travel.length; i++)
-            travel[i] += travel[i-1];
-        int ans = M + P +G;
+        int ans = M + P + G;
         for (int i = garbage.length -1; i >= 0 && (M != 0 || P != 0 || G != 0); i--) {
             for (int j = 0; j < garbage[i].length(); j++) {
                 if(garbage[i].charAt(j) == 'M' && M != 0){
