05362225f530b7830c073981f2754757477bb0cb
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-22 13:56:06 +0530

    Time: 3 ms (99.97%) | Memory: 42.1 MB (40.04%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,49 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int start = 0;
-        int end = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int count = 0;
-
-        for(int i = 0; i < s1.length(); i++) {
-            char temp = s1.charAt(i);
-            if(map.containsKey(temp)) {
-                map.put(temp, map.get(temp) + 1);
-            }
-            else {
-                map.put(temp, 1);
-                count++;
-            }
-        }
-
-        int k = s1.length();
-        while(end < s2.length()) {
-
-            char chEnd = s2.charAt(end);
-            if(map.containsKey(chEnd)) {
-                map.put(chEnd, map.get(chEnd) - 1);
-                if(map.get(chEnd) == 0) {
-                    count--;
-                }
-            }
-            if(end - start + 1 < k) {
-                end++;
-            }
-            else if(end - start + 1 == k) {
-                if(count == 0) {
-                    return true;
-                }
-                char chStart = s2.charAt(start);
-                if(map.containsKey(chStart)) {
-                    map.put(chStart, map.get(chStart) + 1);
-                    if(map.get(chStart) == 1) {
-                        count++;
-                    }
-                }
-                start++;
-                end++;
+        int[] map = new int[128];
+        char[] s1Array = s1.toCharArray();
+        for (int index = 0; index < s1Array.length; index++)
+            map[s1Array[index]]++;
+        int count = s1Array.length;
+        int left = 0, right = 0;
+        char[] s2Array = s2.toCharArray();
+        while (right < s2Array.length) {
+            if (map[s2Array[right++]]-- > 0) 
+                count--;
+            while (count == 0) {
+                if (right-left == s1.length()) return true;
+                if (++map[s2Array[left++]] > 0) count++;
             }
         }
         return false;
     }
+
 }
