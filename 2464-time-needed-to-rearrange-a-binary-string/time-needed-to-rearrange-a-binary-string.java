b90e9f00aae215c38bc8bbb68f8d69b6625bf1ff
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-24 14:45:26 +0530

    Time: 1 ms (100.00%) | Memory: 40.5 MB (81.36%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
@@ -1,23 +1,21 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
         char[] str = s.toCharArray();
-        int zeroCount = 0, waitTime = 0, idx = str.length - 1, i = 0;
-        while(idx >= 0 && str[idx] != '1') {
-            idx--;
-        }
-        while(i < idx && str[i] == '1') {
+        int zeroCount = 0, extraSec = 0, index = str.length - 1, i = 0;
+        while(index >= 0 && str[index] != '1') 
+            index--;
+        while(i < index && str[i] == '1') 
             i++;
-        }
-        for(;i<idx;i++) {
+        for(;i<index;i++) {
             if(str[i] == '0') {
                 zeroCount++;
-                if(str[i + 1] == '0' && waitTime > 0) {
-                    waitTime--;
+                if(str[i + 1] == '0' && extraSec > 0) {
+                    extraSec--;
                 }
             } else if(str[i + 1] == '1') {
-                waitTime++;
+                extraSec++;
             }
         }
-        return zeroCount + waitTime;
+        return zeroCount + extraSec;
     }
 }
