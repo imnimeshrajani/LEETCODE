d65b083332e55f1ae5c8c0d4f151148a70152404
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-24 11:08:11 +0530

    Time: 13 ms (93.83%) | Memory: 44.2 MB (12.33%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -6,14 +6,12 @@ class Solution {
         if(n == 1) return ans;
         for(int i =0; i < arr.length; i++) {
             int j = i;
-            int[] tempStart = new int[2];
-            tempStart[0] = startPos[0];
-            tempStart[1] = startPos[1];
+            int tempVertical = startPos[0], tempHorizontal = startPos[1];
             for(; j < arr.length; j++) {
-                if(arr[j]  == 'L' && tempStart[1] > 0)  tempStart[1]--;
-                else if(arr[j]  == 'R' && tempStart[1] < end) tempStart[1]++;
-                else if(arr[j]  == 'U' && tempStart[0] > 0) tempStart[0]--;
-                else if(arr[j]  == 'D' && tempStart[0] < end) tempStart[0]++;
+                if(arr[j]  == 'L' && tempHorizontal > 0) tempHorizontal--;
+                else if(arr[j]  == 'R' && tempHorizontal < end) tempHorizontal++;
+                else if(arr[j]  == 'U' && tempVertical > 0) tempVertical--;
+                else if(arr[j]  == 'D' && tempVertical < end) tempVertical++;
                 else break;
             }
             ans[i] = j - i;
