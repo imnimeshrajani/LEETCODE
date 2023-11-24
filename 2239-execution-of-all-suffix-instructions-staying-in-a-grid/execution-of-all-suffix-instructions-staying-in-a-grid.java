c50b0541263bb8475fbe0578e4db8b82b66f1425
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-11-24 10:40:18 +0530

    Time: 14 ms (92.95%) | Memory: 44.1 MB (27.75%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] executeInstructions(int n, int[] startPos, String s) {
         int L = -1, R = 1, U = -1, D = 1, end = n - 1;
+        char[] arr = s.toCharArray();
         int[] ans = new int[s.length()];
         if(n == 1) return ans;
-        for(int i =0; i < s.length(); i++) {
+        for(int i =0; i < arr.length; i++) {
             int j = i;
             int[] tempStart = new int[2];
             tempStart[0] = startPos[0];
             tempStart[1] = startPos[1];
-            for(; j < s.length(); j++) {
-                if(s.charAt(j)  == 'L' && tempStart[1] > 0)  tempStart[1]--;
-                else if(s.charAt(j)  == 'R' && tempStart[1] < end) tempStart[1]++;
-                else if(s.charAt(j)  == 'U' && tempStart[0] > 0) tempStart[0]--;
-                else if(s.charAt(j)  == 'D' && tempStart[0] < end) tempStart[0]++;
+            for(; j < arr.length; j++) {
+                if(arr[j]  == 'L' && tempStart[1] > 0)  tempStart[1]--;
+                else if(arr[j]  == 'R' && tempStart[1] < end) tempStart[1]++;
+                else if(arr[j]  == 'U' && tempStart[0] > 0) tempStart[0]--;
+                else if(arr[j]  == 'D' && tempStart[0] < end) tempStart[0]++;
                 else break;
             }
-            
             ans[i] = j - i;
         }
         return ans;
