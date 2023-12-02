5381f27e370ace16ec73e13571ff10c83c28b4d1
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-02 12:44:22 +0530

    Time: 5 ms (95.43%) | Memory: 44.3 MB (42.47%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,15 +1,16 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int[] alphabet = new int[26];
+        char[] ch = chars.toCharArray();
         int ans = 0;
-        for(int i=0;i<chars.length();i++)
-            alphabet[chars.charAt(i)-'a']++;
+        for(int i = 0; i < ch.length; i++)
+            alphabet[ch[i] - 'a']++;
 
         for(String s : words) {
             int[] c = new int[26];
-            int i =0;
-            while(i<s.length()) {
-                int x = s.charAt(i)-'a';
+            int i = 0;
+            while(i < s.length()) {
+                int x = s.charAt(i) - 'a';
                 if(++c[x] > alphabet[x]) break;
                 i++;
             }
