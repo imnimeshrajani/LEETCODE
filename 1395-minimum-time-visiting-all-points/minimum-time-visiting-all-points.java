4212c229966eb999948e1833ddd096751e9f53b4
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-03 11:30:13 +0530

    Time: 1 ms (95.62%) | Memory: 42.9 MB (56.82%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i = 0; i < points.length - 1; i++) {
             int diff1 = Math.abs(points[i][0] - points[i+1][0]);
             int diff2 = Math.abs(points[i][1] - points[i+1][1]);
-            if(diff1 < diff2) ans += diff2;
-            else ans += diff1;
+            ans += Math.max(diff1, diff2);
         }
         return ans;
     }
