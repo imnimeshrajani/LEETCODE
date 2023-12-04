ad884382e47e685340767dc8f8725b56873a9481
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-04 16:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (62.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,9 +1,4 @@
 class Solution {
-    public int climbStairs(int n) {
-        return (n==1 || n==2 || n==3) ? n : fibo(n, 2,3);     
-    }
-
-    int fibo(int n, int i, int j) {
-        return n == 4 ? i + j : fibo(n-1, j, i+j);
-    }
+    public int climbStairs(int n) { return (n==1 || n==2 || n==3) ? n : fibo(n, 2,3); }
+    int fibo(int n, int i, int j) { return n == 4 ? i + j : fibo(n-1, j, i+j); }
 }
