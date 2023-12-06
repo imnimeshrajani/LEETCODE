999a3779e86d20fd3def0bb75570d4b8f07136b5
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-06 11:50:13 +0530

    Time: 1 ms (98.23%) | Memory: 61.6 MB (7.96%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
@@ -2,9 +2,9 @@ class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length, ans = 0, index = 0;
         if(n % 2 == 0 && m % 2 == 0) return ans;
-        if(n % 2 != 0 && m % 2 != 0) return xor(nums1) ^ xor(nums2);
-        if(n % 2 == 0 && m % 2 != 0) return xor(nums1);
-        return xor(nums2);
+        if(n % 2 == 0) return xor(nums1);
+        if(m % 2 == 0) return xor(nums2);
+        return xor(nums1) ^ xor(nums2);
     }
     public int xor(int[] a) {
         int value = 0;
