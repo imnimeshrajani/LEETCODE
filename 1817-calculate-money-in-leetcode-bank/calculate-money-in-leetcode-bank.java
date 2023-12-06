f08255c3c515aaced71607d2565c08c85523ea69
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-06 10:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (25.95%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,7 +1,6 @@
 class Solution {
     public int totalMoney(int n) {
-        int e = n % 7;
-        n/=7;
+        int e = n % 7; n/=7;
         return 28 * n + 7 * (n*(n-1)/2) + n*e + (e*(e+1)/2);
     }
 }
