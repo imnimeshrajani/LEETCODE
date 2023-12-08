970dbe679767a81fd3a56a3278c138e9c5cd480d
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-08 13:45:27 +0530

    Time: 2 ms (99.56%) | Memory: 44.1 MB (63.12%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String convert(String s, int n) {
+        if(n == 1) return s;
+        char[] ch = s.toCharArray();
+        StringBuilder ans = new StringBuilder();
+        for(int i = 1; i <= n; i++){
+            int index = i - 1;
+            boolean up = true;
+            while (index < ch.length){
+                ans.append(ch[index]);
+                if (i==1) index += 2*(n-i);
+                else if (i==n) index += 2*(i-1);
+                else {
+                    index += up ? 2*(n-i) : 2*(i-1);
+                    up=!up;
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
