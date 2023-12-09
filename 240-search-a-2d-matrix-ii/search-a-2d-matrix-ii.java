9a60cbd5702bbf3d2c24fda4c2152cfc07d21111
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-09 09:40:16 +0530

    Time: 5 ms (86.46%) | Memory: 48.9 MB (79.59%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0, j = matrix[0].length - 1;
         while(i < matrix.length && j >= 0) {
             if(matrix[i][j] == target) return true;
-            if(matrix[i][j] < target) i++;
+            else if(matrix[i][j] < target) i++;
             else j--;
         }
         return false;
