250b655221b819013d637b680ff466ce1b8820f4
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-14 15:10:37 +0530

    Time: 4 ms (98.71%) | Memory: 73.8 MB (38.51%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
-        int[] colSum = new int[grid[0].length], rowSum = new int[grid.length];
         int n = grid.length, m = grid[0].length, sum = 0;
+        int[] colSum = new int[m], rowSum = new int[n];
         for (int i = 0; i < n; i++) {
             sum = 0;
             for (var j : grid[i]) {
-                if(j == 0) sum -=1; 
-                else sum +=1;
+                sum +=j;
             }
-            rowSum[i] = sum;
+            rowSum[i] = (2*sum) - m;
         }
         for (int i = 0; i < m; i++) {
             sum = 0;
-            for (var j : grid) {
-                if(j[i] == 0) sum -=1; 
-                else sum +=1;
+            for(int[] j : grid){
+                sum += j[i];
             }
-            colSum[i] = sum;
+            colSum[i] = (2*sum) - n;
         }
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
