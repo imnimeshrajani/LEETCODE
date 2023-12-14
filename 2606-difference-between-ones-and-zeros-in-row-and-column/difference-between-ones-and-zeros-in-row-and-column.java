553a2bad01d9d25d7ea8479ac676e1113e348df7
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-14 15:11:13 +0530

    Time: 4 ms (98.71%) | Memory: 72.9 MB (55.02%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -4,23 +4,17 @@ class Solution {
         int[] colSum = new int[m], rowSum = new int[n];
         for (int i = 0; i < n; i++) {
             sum = 0;
-            for (var j : grid[i]) {
-                sum +=j;
-            }
+            for (var j : grid[i]) sum +=j;
             rowSum[i] = (2*sum) - m;
         }
         for (int i = 0; i < m; i++) {
             sum = 0;
-            for(int[] j : grid){
-                sum += j[i];
-            }
+            for(int[] j : grid) sum += j[i];
             colSum[i] = (2*sum) - n;
         }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++) 
+            for(int j = 0; j < m; j++) 
                 grid[i][j] = rowSum[i] + colSum[j];
-            }
-        }
         return grid;
     }
 }
