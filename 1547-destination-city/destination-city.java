c5270040ebe46ba119ecdd90b2342d09c331e9bf
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-15 08:01:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (77.75%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -2,12 +2,8 @@ class Solution {
     public String destCity(List<List<String>> paths) {
         Map<String, String> map = new HashMap<>();
         String start = paths.get(0).get(1);
-        for(int i = 0; i < paths.size(); i++){
-            map.put(paths.get(i).get(0), paths.get(i).get(1));
-        }
-        for(int i = 0; i < paths.size(); i++){
-            if(map.containsKey(start)) start = map.get(start);
-        }
+        for (List<String> i : paths) map.put(i.get(0), i.get(1));
+        while (map.containsKey(start)) start = map.get(start);
         return start;
     }
 }
