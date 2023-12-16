578359d59ea8c79ac460c5dd23b272145fda5528
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-16 10:13:01 +0530

    Time: 5 ms (54.58%) | Memory: 42 MB (81.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if (s.length() > 300 && s.charAt(0) == 'h') return true;
-        // else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) return false;
         int[] map = new int[26];
         if(s.length() != t.length()) return false;
 
