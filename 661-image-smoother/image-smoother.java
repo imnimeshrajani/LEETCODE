4eda3cb6c90d261045dd5fa4d0a9106bdcb8766a
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-19 10:53:54 +0530

    Time: 6 ms (80.11%) | Memory: 44.6 MB (46.78%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -4,15 +4,38 @@ class Solution {
         int[][] ans = new int[n][m];
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                int sum = 0, count = 0;
-                for(int k = -1; k <= 1; k++) {
-                    for(int l = -1; l <= 1; l++) {
-                        int nx = i + k;
-                        int ny = j + l;
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                        sum += img[nx][ny];
-                        count++;
-                    }
+                int sum = img[i][j], count = 1;
+                if(i-1>=0) {
+                    count++;
+                    sum += img[i-1][j];
+                }
+                if(j-1>=0) {
+                    count++;
+                    sum += img[i][j-1];
+                }
+                if(j+1<m) {
+                    count++;
+                    sum += img[i][j+1];
+                }
+                if(i-1>=0 && j-1>=0) {
+                    count++;
+                    sum += img[i-1][j-1];
+                }
+                if(i-1>=0 && j+1<m) {
+                    count++;
+                    sum += img[i-1][j+1];
+                }
+                if(i+1 < n) {
+                    count++;
+                    sum += img[i+1][j];
+                }
+                if(i+1<n && j+1 < m) {
+                    count++;
+                    sum += img[i+1][j+1];
+                }
+                if(i+1 < n && j-1 >= 0) {
+                    count++;
+                    sum += img[i+1][j-1];
                 }
                 ans[i][j] = sum/count;
             }
