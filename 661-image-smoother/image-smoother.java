7cc6d50833bdac97bd873a06640a93fa359945b1
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-19 10:59:10 +0530

    Time: 3 ms (99.16%) | Memory: 45.2 MB (8.40%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -4,42 +4,45 @@ class Solution {
         int[][] ans = new int[n][m];
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                int sum = img[i][j], count = 1;
-                if(i-1>=0) {
-                    count++;
-                    sum += img[i-1][j];
-                }
-                if(j-1>=0) {
-                    count++;
-                    sum += img[i][j-1];
-                }
-                if(j+1<m) {
-                    count++;
-                    sum += img[i][j+1];
-                }
-                if(i-1>=0 && j-1>=0) {
-                    count++;
-                    sum += img[i-1][j-1];
-                }
-                if(i-1>=0 && j+1<m) {
-                    count++;
-                    sum += img[i-1][j+1];
-                }
-                if(i+1 < n) {
-                    count++;
-                    sum += img[i+1][j];
-                }
-                if(i+1<n && j+1 < m) {
-                    count++;
-                    sum += img[i+1][j+1];
-                }
-                if(i+1 < n && j-1 >= 0) {
-                    count++;
-                    sum += img[i+1][j-1];
-                }
-                ans[i][j] = sum/count;
+                ans[i][j] = solve(img, i, j);
             }
         }
         return ans;
+    }
+    public int solve(int[][] img, int i,int j) {
+        int sum = img[i][j], count = 1, n = img.length, m = img[0].length;
+        if(i-1>=0) {
+            count++;
+            sum += img[i-1][j];
+        }
+        if(j-1>=0) {
+            count++;
+            sum += img[i][j-1];
+        }
+        if(j+1<m) {
+            count++;
+            sum += img[i][j+1];
+        }
+        if(i-1>=0 && j-1>=0) {
+            count++;
+            sum += img[i-1][j-1];
+        }
+        if(i-1>=0 && j+1<m) {
+            count++;
+            sum += img[i-1][j+1];
+        }
+        if(i+1 < n) {
+            count++;
+            sum += img[i+1][j];
+        }
+        if(i+1<n && j+1 < m) {
+            count++;
+            sum += img[i+1][j+1];
+        }
+        if(i+1 < n && j-1 >= 0) {
+            count++;
+            sum += img[i+1][j-1];
+        }
+        return sum/count;
     }
 }
