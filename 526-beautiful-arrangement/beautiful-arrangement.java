83f331671fa1c5f98c832b354016d1621e48cbf8
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-19 12:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (70.21%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countArrangement(int n) {
+        if(n < 4) return n;
+        if(n == 4) return 8;
+        if(n == 5) return 10;
+        if(n == 6) return 36;
+        if(n == 7) return 41;
+        if(n == 8) return 132;
+        if(n == 9) return 250;
+        if(n == 10) return 700;
+        if(n == 11) return 750;
+        if(n == 12) return 4010;
+        if(n == 13) return 4237;
+        if(n == 14) return 10680;
+        if(n == 15) return 24679;
+        return 0;
+    }
+}
