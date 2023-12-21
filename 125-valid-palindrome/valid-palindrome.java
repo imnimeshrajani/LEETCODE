19b97cf888ff38635aa7c13efc41f6ea0f909993
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-21 14:46:41 +0530

    Time: 2 ms (99.34%) | Memory: 43.1 MB (56.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length()-1;
+        char[] ch = s.toCharArray();
         while(i<j){
-            char start = s.charAt(i), end = s.charAt(j);
-            if(!isValid(start)) i++;
-            else if(!isValid(end)) j--;
+            if(!isValid(ch[i])) i++;
+            else if(!isValid(ch[j])) j--;
             else{
-                start = (start >= 'A' && start <= 'Z') ? start += ' ' : start;
-                end = (end >= 'A' && end <= 'Z') ? end += ' ' : end;
-                if(start != end) return false;
+                ch[i] = (ch[i] >= 'A' && ch[i] <= 'Z') ? ch[i] += ' ' : ch[i];
+                ch[j] = (ch[j] >= 'A' && ch[j] <= 'Z') ? ch[j] += ' ' : ch[j];
+                if(ch[i] != ch[j]) return false;
                 i++;
                 j--;
             }  
