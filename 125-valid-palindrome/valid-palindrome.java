c48d653ecf962479df78872d235470e23eb4ea33
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-21 14:42:27 +0530

    Time: 2 ms (99.34%) | Memory: 42.7 MB (57.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) return true;
-        s = s.toLowerCase();
-        char[] ch = s.toCharArray();
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < ch.length; i++) 
-            if(ch[i] != 32  && (ch[i] >= 'a' && ch[i] <='z') || (ch[i] >= '0' && ch[i] <='9')) str.append(s.charAt(i));
-        int i = 0, j = str.length()-1;
-        while(i<j) if(str.charAt(i++) != str.charAt(j--)) return false;
+        int i = 0, j = s.length()-1;
+        while(i<j){
+            char start = s.charAt(i), end = s.charAt(j);
+            if(!isValid(start)) i++;
+            else if(!isValid(end)) j--;
+            else{
+                start = (start >= 'A' && start <= 'Z') ? start += ' ' : start;
+                end = (end >= 'A' && end <= 'Z') ? end += ' ' : end;
+                if(start != end) return false;
+                i++;
+                j--;
+            }  
+        }
         return true;
+    }
+    private boolean isValid(char ch){
+        return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ||(ch >= '0' && ch <= '9');
     }
 }
