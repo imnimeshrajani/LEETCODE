2969537c3c7a16fad64ee1147e53bab0b9e113ab
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-23 11:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (23.83%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,14 +1,27 @@
 class Solution {
+    ArrayList<String> times = new ArrayList<>();
     public List<String> readBinaryWatch(int turnedOn) {
-        ArrayList<String> result = new ArrayList();
-        for(int hour=0; hour<12; hour++){
-            for(int minute=0; minute<60; minute++){
-                if(Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn){
-                    if(minute<10) result.add(String.format("%d:0%d",hour,minute));
-                    else result.add(String.format("%d:%d",hour,minute));
-                }
-            }
+        int[] nums = {8, 4, 2, 1, 32, 16, 8, 4, 2, 1};
+        bactrack(nums, 0, 0, nums.length - 1, turnedOn, 0);
+        return times;
+    }
+
+    public void bactrack(int[] nums, int hour, int minute, int pos, int turnedOn, int curOn) {
+        if (curOn == turnedOn && hour <= 11 && minute <= 59) {
+            StringBuilder b = new StringBuilder();
+            b.append(hour).append(":").append(minute < 10 ? "0" : "").append(minute);
+            times.add(b.toString());
+            return;
         }
-        return result;
+
+        for (int i = pos; i >= 0; i--) {
+            if (0 <= i && i <= 3) hour += nums[i];
+            else minute += nums[i];
+
+            bactrack(nums, hour, minute, i - 1, turnedOn, curOn + 1);
+
+            if (0 <= i && i <= 3) hour -= nums[i];
+            else minute -= nums[i];
+        }
     }
 }
