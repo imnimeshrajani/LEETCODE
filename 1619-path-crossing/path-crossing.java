d6a1d14b6d8b15d5cca06020b9da34e438b65ecc
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-23 10:32:21 +0530

    Time: 1 ms (96.48%) | Memory: 41.8 MB (8.15%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,22 +1,26 @@
 class Solution {
+    public int x = 0, y = 0;
     public boolean isPathCrossing(String path) {
         char[] ch = path.toCharArray();
         Set<String> set = new HashSet<>();
         set.add("0,0");
         boolean flag = false;
-        int x = 0, y = 0;
         for(int i = 0; i < ch.length; i++) {
-            if(ch[i] == 'N') x++;
-            if(ch[i] == 'S') x--;
-            if(ch[i] == 'E') y++;
-            if(ch[i] == 'W') y--;
+            String s =solve(ch[i]);
+            if(set.contains(s)) return true;
+            else set.add(s);
+        }
+        return false;
+    }
+    public String solve(char ch) {
+            if(ch == 'N') x++;
+            if(ch == 'S') x--;
+            if(ch == 'E') y++;
+            if(ch == 'W') y--;
             StringBuilder str = new StringBuilder();
             str.append(x);
             str.append(',');
             str.append(y);
-            if(set.contains(str.toString())) return true;
-            else set.add(str.toString());
-        }
-        return false;
+            return str.toString();
     }
 }
