19be127601088c03db73737e67a9447e50bbb251
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-27 12:06:53 +0530

    Time: 11 ms (31.14%) | Memory: 52 MB (26.97%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,7 +1,27 @@
 class Solution {
-    public List<Integer> grayCode(int n) {
-        var list = new ArrayList();
-        for(var i = 0; i < (1<<n); i++) list.add(i ^ (i>>1));
-        return list;
+    // public List<Integer> grayCode(int n) {
+    //     var list = new ArrayList();
+    //     for(var i = 0; i < (1 << n); i++) list.add(i ^ (i>>1));
+    //     return list;
+    // }
+
+    public List<Integer> grayCode(int n) {    
+        if(n == 1) {
+            List<Integer> list = new ArrayList<>();
+            list.add(0);
+            list.add(1);
+            return list;
+        }
+        
+        List<Integer> pres = grayCode(n-1);
+        List<Integer> mres = new ArrayList<>();
+        
+        for(int i=0;i<pres.size();i++)
+            mres.add(0+2*pres.get(i));
+        
+        for(int i=pres.size()-1;i>=0;i--)
+            mres.add(1+2*pres.get(i));
+        
+        return mres;
     }
 }
