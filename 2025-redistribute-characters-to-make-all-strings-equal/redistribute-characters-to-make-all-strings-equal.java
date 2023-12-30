de5be9db016f5d8418106dc3a41b3b6679244e0e
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2023-12-30 10:45:51 +0530

    Time: 4 ms (43.02%) | Memory: 44.3 MB (6.04%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean makeEqual(String[] words) {
-        int totalCharCount = 0;
-		for (String s : words) {
-			totalCharCount += s.length();
-		}
-		if (totalCharCount % words.length != 0) {
-			return false;
-		}
-
-		int[] arr = new int[26];
+        int[] arr = new int[26];
 		for (String s : words) {
 			for (char c : s.toCharArray()) {
 				arr[c - 'a']++;
