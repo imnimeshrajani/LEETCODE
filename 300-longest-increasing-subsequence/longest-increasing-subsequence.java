c5f91faa116655d6369acec941e89f8aa02a21a2
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-05 09:06:32 +0530

    Time: 5 ms (89.05%) | Memory: 44.4 MB (18.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,33 +2,26 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(nums[0]);
-        int len=1;
+        int ans = 1;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]>list.get(list.size()-1)){
+            if(nums[i] > list.get(list.size()-1)){
                 list.add(nums[i]);
-                len++;
-            }else{
-                int floor=binarySearch(list, nums[i]);
-                list.set(floor,nums[i]);
-            }
+                ans++;
+            }else list.set(binarySearch(list, nums[i]),nums[i]);
+    
              
         }
-        return len;
+        return ans;
         
     }
     static int binarySearch(ArrayList<Integer> ans, int key) {
-        int low = 0;
-        int high = ans.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int start = 0, end = ans.size() - 1, mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
             if (ans.get(mid) == key) return mid;
-            else if (ans.get(mid) < key) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            else if (ans.get(mid) < key) start = mid + 1;
+            else end = mid - 1;
         }
-        return high + 1;
-        
+        return end + 1;
     }
 }
