2efa164158de154c56ca5d4bd29fa7fee467a7ab
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-10 13:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (78.63%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,24 @@
 class Solution {
-     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        backtrack(list, "", 0, 0, n);
-        return list;
-    }
-    
-    public void backtrack(List<String> list, String str, int open, int close, int max){
-        
-        if(str.length() == max*2){
-            list.add(str);
-            return;
-        }
-        
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
-        if(close < open)
-            backtrack(list, str+")", open, close+1, max);
-    }
+    public static List<String> generateParenthesis(int n) {
+		List<String> list = new ArrayList<>();
+
+		helper(list, new StringBuilder("("), n - 1, n);
+		return list;
+	}
+
+	public static void helper(List<String> list, StringBuilder sb, int open, int close) {
+		if (open == 0 && close == 0) {
+			list.add(sb.toString());
+		}
+
+		if (open != 0 && close != 0 && open <= close) {
+			helper(list, sb.append('('), open - 1, close);
+			sb.setLength(sb.length() - 1);
+		}
+
+		if (close != 0) {
+			helper(list, sb.append(')'), open, close - 1);
+			sb.setLength(sb.length() - 1);
+		}
+	}
 }
