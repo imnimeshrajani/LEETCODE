c0bd8f712c1f678a3e3a2a52bb4b46d41d6a18fc
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-12 11:56:07 +0530

    Time: 2 ms (95.53%) | Memory: 41.8 MB (28.59%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        char[] ch = s.toCharArray();
-        Set<Character> set = new HashSet<>();
-        set.add('A');
-        set.add('E');
-        set.add('I');
-        set.add('O');
-        set.add('U');
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-        int count1 = 0, count2 = 0;
-        for(int i = 0; i < ch.length/2; i++) if(set.contains(ch[i])) count1++;
-        for(int i = ch.length/2; i < ch.length; i++) if(set.contains(ch[i])) count2++;
-        return count1 == count2;
+        int mid = s.length() / 2;
+        String firstHalf = s.substring(0, mid);
+        String secondHalf = s.substring(mid);
+        return countVowels(firstHalf) == countVowels(secondHalf);
+    }
+    
+    private int countVowels(String str) {
+        int count = 0;
+        for (char ch : str.toCharArray()) if ("AEIOUaeiou".indexOf(ch) != -1) count++;
+        return count;
     }
 }
