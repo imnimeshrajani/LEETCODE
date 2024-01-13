dd1e6a0f58cee7a2cd3292ca2219dd52e6fc6283
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-13 09:19:58 +0530

    Time: 11 ms (46.76%) | Memory: 41 MB (35.88%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        // Start solving sudoku from the first cell
+        solve(board, 0, 0);
+    }
+
+    private boolean solve(char[][] board, int row, int col) {
+        if (row == board.length) return true;
+        
+        if (col == board.length) return solve(board, row + 1, 0);
+        if (board[row][col] != '.') return solve(board, row, col + 1);
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValidPlacement(board, row, col, num)) {
+                board[row][col] = num;
+                if (solve(board, row, col + 1)) return true;
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidPlacement(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < board.length; i++){
+            int subgridRow = 3 * (row / 3) + i / 3; 
+            int subgridCol = 3 * (col / 3) + i % 3;
+            if (board[i][col] == num || board[row][i] == num || board[subgridRow][subgridCol] == num) return false;
+        }
+        return true;
+    }
+}
