6ef7c39fc2ad14d11685fd13e06aae4091f52390
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-17 13:41:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (5.39%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,11 +1,28 @@
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i : arr) map.put(i, map.getOrDefault(i, 0)+1);
+//         for(var val : map.entrySet()) {
+//             int v = val.getValue();
+//             val.setValue(-1);
+//             if(map.containsValue(v)) return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i : arr) map.put(i, map.getOrDefault(i, 0)+1);
-        for(var val : map.entrySet()) {
-            int v = val.getValue();
-            val.setValue(-1);
-            if(map.containsValue(v)) return false;
+        int[] freq = new int[2001];
+        boolean[] used = new boolean[1001];
+        for (int i : arr) freq[1000 + i]++;
+        for (int i : arr) {
+            int count = freq[1000 + i];
+            freq[1000 + i] = 0;
+            if (count > 0) {
+                if (used[count]) return false;
+                used[count] = true;
+            }
         }
         return true;
     }
