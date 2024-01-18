5a4ca31b3dec45d038e70aaeceb233c76b63e302
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-18 09:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (52.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,11 @@
 class Solution {
     public int climbStairs(int n) { 
         int a = 1, b = 1, i = 1;
-        while(i++ !=n){
-            a = a + b;
-            a = a ^ b;
-            b = a ^ b;
-            a = a ^ b;
+        while(i++ != n){
+            a += b;
+            a ^= b;
+            b ^= a;
+            a ^= b;
         }
         return b;
     }
