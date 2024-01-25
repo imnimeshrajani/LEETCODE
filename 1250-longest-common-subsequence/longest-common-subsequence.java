d182cd0c1a3c14f8a84497019a05d40674a386b6
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-25 14:29:55 +0530

    Time: 6 ms (99.97%) | Memory: 41.4 MB (97.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,42 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int length1 = text1.length(), length2 = text2.length();
-        int[][] dp = new int[length1 + 1][length2 + 1];
-        for (int i = 1; i <= length1; ++i) {
-            for (int j = 1; j <= length2; ++j) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        if(text1.equals(text2))
+           return text1.length();
+         char tb1[] = text1.toCharArray();
+        char tb2[] = text2.toCharArray();
+
+        if(tb1.length > tb2.length) {
+            int dp[] = new int[tb1.length + 1];
+            for(int i = 0; i < tb2.length; i++) {
+                int pc = 0, prc;
+                for(int j = 0; j < tb1.length; j++) {
+                    prc = pc;
+                    pc = dp[j + 1];
+                    if(tb2[i] == tb1[j]) {
+                        dp[j + 1] = prc + 1;
+                    } else {
+                        dp[j + 1] = Math.max(pc, dp[j]);
+                    }
                 }
             }
+
+            return dp[tb1.length];
+        } else {
+            int dp[] = new int[tb2.length + 1];
+            for(int i = 0; i < tb1.length; i++) {
+                int pc = 0, prc;
+                for(int j = 0; j < tb2.length; j++) {
+                    prc = pc;
+                    pc = dp[j + 1];
+                    if(tb1[i] == tb2[j]) {
+                        dp[j + 1] = prc + 1;
+                    } else {
+                        dp[j + 1] = Math.max(pc, dp[j]);
+                    }
+                }
+            }
+
+            return dp[tb2.length];
         }
-        return dp[length1][length2];
     }
 }
