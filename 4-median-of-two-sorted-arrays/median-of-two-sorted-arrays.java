95a791795db7470529be59d2cefba8dd2b8632af
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-01-27 10:52:02 +0530

    Time: 2 ms (68.55%) | Memory: 45.5 MB (59.08%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -28,11 +28,8 @@ class Solution {
         int i = 0, j = 0, m1 = 0, m2 = 0;
         for (int count = 0; count <= (n + m) / 2; count++) {
             m2 = m1;
-            if (i != n && j != m) {
-                if (nums1[i] > nums2[j]) m1 = nums2[j++];
-                else m1 = nums1[i++];
-            } else if (i < n) m1 = nums1[i++];
-            else m1 = nums2[j++];
+            if (i != n && j != m) m1 = (nums1[i] > nums2[j]) ? nums2[j++] : nums1[i++];
+            else m1 = (i < n) ? nums1[i++] : nums2[j++];
         }
         return (double) (n + m) % 2 == 1 ? m1 : ((double) m1 + (double) m2) / 2.0;
     }
