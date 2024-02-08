67e5449a068080c3879fb05cc0b871ad5ddc0173
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-02-08 11:24:39 +0530

    Time: 26 ms (83.92%) | Memory: 42.6 MB (64.19%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,13 +1,11 @@
 public class Solution {
     public int numSquares(int n) {
-        int[] freq = new int[n+1];
+        int[] freq = new int[++n];
         Arrays.fill(freq, Integer.MAX_VALUE);
         freq[0] = 0;
-        for(int i = 0; i <= n; i++) {
-            for(int j = 1; i + (j * j) <= n; j++) {
+        for(int i = 0; i < n; i++) 
+            for(int j = 1; i + (j * j) < n; j++) 
                 freq[i + (j * j)] = Math.min(freq[i + (j * j)], freq[i]+1);
-            }
-        }
-        return freq[n];
+        return freq[--n];
     }
 }
