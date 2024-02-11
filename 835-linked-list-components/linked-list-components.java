5cd864cff2090c3be8c8e5a449121a8a74abbb62
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-02-11 13:21:25 +0530

    Time: 2 ms (98.98%) | Memory: 45.3 MB (71.11%) - LeetSync

--- 835-linked-list-components/linked-list-components.java
+++ 835-linked-list-components/linked-list-components.java
@@ -1,26 +1,37 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public int numComponents(ListNode head, int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        boolean flag = false;
-        for (int i : nums) map.put(i, 0);
-        while(head != null) {
-            if(map.containsKey(head.val)) {
-                if(!flag) count++;
-                flag = true;
-            } else flag = false;
-            head = head.next;
+        boolean[] arr = new boolean[10001];
+        for (int i : nums) arr[i] = true;
+        int result = 0;
+        while (head != null) {
+            if (arr[head.val]) {
+                while (head != null && arr[head.val]) head = head.next;
+                result++;
+            } else head = head.next;
         }
-        return count;
+        return result;
+
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int count = 0;
+        // boolean flag = false;
+        // for (int i : nums) map.put(i, 0);
+        // while(head != null) {
+        // if(map.containsKey(head.val)) {
+        // if(!flag) count++;
+        // flag = true;
+        // } else flag = false;
+        // head = head.next;
+        // }
+        // return count;
     }
 }
