297b596b91d06a03361463737345645a828d876d
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-02-18 16:04:01 +0530

    Time: 2 ms (99.41%) | Memory: 41.8 MB (97.29%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -2,10 +2,10 @@ class Solution {
     public String multiply(String num1, String num2) {
         int m = num1.length(), n = num2.length();
         int[] pos = new int[m + n];
-
+        char[] arr1 = num1.toCharArray(), arr2 = num2.toCharArray();
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int mul = (arr1[i] - '0') * (arr2[j] - '0');
                 int p1 = i + j, p2 = i + j + 1;
                 int sum = mul + pos[p2];
 
