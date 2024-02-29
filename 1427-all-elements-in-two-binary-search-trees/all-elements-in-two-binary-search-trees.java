ac72709315f4140795a6670bbc505249df3dc790
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-02-29 11:08:06 +0530

    Time: 17 ms (74.29%) | Memory: 46.6 MB (16.29%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -16,15 +16,34 @@
 class Solution {
     List<Integer> ans = new ArrayList<>();
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        helper(root1);
-        helper(root2);
-        Collections.sort(ans);
-        return ans;
+        List<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
+        helper(root1, list1);
+        helper(root2, list2);
+        return merge(list1, list2);
     }
-    void helper(TreeNode root) {
+    void helper(TreeNode root, List<Integer> list) {
         if(root == null) return;
-        ans.add(root.val);
-        helper(root.left);
-        helper(root.right);
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+    }
+    List<Integer> merge(List<Integer> list1, List<Integer> list2) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while(i<list1.size() && j<list2.size()){
+            if(list1.get(i) < list2.get(j))
+                list.add(list1.get(i++));
+            else
+                list.add(list2.get(j++));
+        }
+
+        while(i<list1.size()) 
+            list.add(list1.get(i++));
+
+        while(j<list2.size()) 
+            list.add(list2.get(j++));
+
+        return list;
     }
 }
