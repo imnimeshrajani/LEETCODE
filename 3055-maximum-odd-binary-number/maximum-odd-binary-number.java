72d04e7cbd722a8330f80b931048a2d64975b602
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-01 06:59:12 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (32.38%) - LeetSync

---
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int count = 0;
+        for(char ch : s.toCharArray()) if(ch == '1') count++;
+        StringBuilder ans = new StringBuilder();
+        if(count > 1) for(int i = 1; i < count; i++) ans.append('1');
+        for(int i = 0; i < s.length() - count; i++) ans.append('0');
+        ans.append('1');
+        return ans.toString();
+    }
+}
