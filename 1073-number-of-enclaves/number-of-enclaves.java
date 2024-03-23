06dbae70a034d522fdeb4ad4eb93a4f341ef0db0
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-23 10:40:06 +0530

    Time: 5 ms (100.00%) | Memory: 56.7 MB (56.02%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        for (int i = 0; i < grid[0].length; i++) {
+            helper(grid, 0, i);
+            helper(grid, grid.length - 1, i);
+        }
+        for (int i = 0; i < grid.length; i++) {
+            helper(grid, i, grid[0].length - 1);
+            helper(grid, i, 0);
+        }
+        int ans = 0;
+        for (int[] i : grid) {
+            for (int j : i) {
+                ans += j;
+            }
+        }
+        return ans;
+    }
+
+    void helper(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
+            return;
+        grid[i][j] = 0;
+        helper(grid, i + 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i - 1, j);
+        helper(grid, i, j - 1);
+
+    }
+}
