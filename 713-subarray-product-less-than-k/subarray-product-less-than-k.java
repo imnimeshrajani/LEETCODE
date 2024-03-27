3f49cbd78d3a7eab0f91d1bfea5eafcea8e519eb
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-27 14:18:26 +0530

    Time: 4 ms (100.00%) | Memory: 47.6 MB (63.17%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -3,13 +3,10 @@ class Solution {
        int l = 0, r = 0, ans = 0, prod = 1;
         while (r < nums.length) {
             prod *= nums[r];
-            while (prod >= k && l <= r) {
-                prod /= nums[l];
-                l++;
-                
-            }
-            ans += r - l + 1;
-            r++;
+            while (prod >= k && l <= r) 
+                prod /= nums[l++];
+
+            ans += r++ - l + 1;
         }
         return ans;
     }
