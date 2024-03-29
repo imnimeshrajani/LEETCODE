eca9021f25fc682b796a0e87c51cb602db33f4ab
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-29 14:41:56 +0530

    Time: 11 ms (100.00%) | Memory: 63.9 MB (49.46%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
@@ -1,19 +1,29 @@
 class Solution {
     public int longestEqualSubarray(List<Integer> nums, int k) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int ans = 0;
-        for (int i = 0; i< nums.size(); i++) {
-            map.putIfAbsent(nums.get(i), new ArrayList<>());
-            map.get(nums.get(i)).add(i);
+        int n = nums.size(), left = 0, right = 0, ans = 0;
+        int[] count = new int[n + 1];
+        while (right < n) {
+            if (right - left - ans > k)
+               count[nums.get(left++)]--;  
+               
+            ans = Math.max(ans, ++count[nums.get(right++)]);   
         }
-        for (int key : map.keySet()) {
-            List<Integer> list = map.get(key);
-            int left = 0, right = 0;
-            while (right < list.size()) {
-                while(list.get(right) - list.get(left) - (right - left) > k) left++;
-                ans = Math.max(ans, right++ - left + 1);
-            }
-        }
+        
         return ans;
+        // Map<Integer, List<Integer>> map = new HashMap<>();
+        // int ans = 0;
+        // for (int i = 0; i< nums.size(); i++) {
+        //     map.putIfAbsent(nums.get(i), new ArrayList<>());
+        //     map.get(nums.get(i)).add(i);
+        // }
+        // for (int key : map.keySet()) {
+        //     List<Integer> list = map.get(key);
+        //     int left = 0, right = 0;
+        //     while (right < list.size()) {
+        //         while(list.get(right) - list.get(left) - (right - left) > k) left++;
+        //         ans = Math.max(ans, right++ - left + 1);
+        //     }
+        // }
+        // return ans;
     }
 }
