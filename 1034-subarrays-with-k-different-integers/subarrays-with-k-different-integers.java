c8a96681c6e688c0cae84498a5543886685a9760
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-30 11:47:50 +0530

    Time: 4 ms (99.30%) | Memory: 46.8 MB (67.68%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,6 +1,22 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1); 
+        // return helper(nums, k) - helper(nums, k - 1); 
+        int[] numFreq = new int[nums.length+1];
+        int distinct = 0, start = 0, minEnd = 0, total = 0;
+        while (distinct == k || minEnd < nums.length) {
+            while (distinct < k && minEnd < nums.length) 
+			    if (numFreq[nums[minEnd++]]++ == 0) 
+                distinct++;
+            int maxEnd = minEnd;
+            while (maxEnd < nums.length && numFreq[nums[maxEnd]] > 0)
+			    maxEnd++;
+            while (distinct == k) {
+                if (numFreq[nums[start++]]-- == 1)
+                distinct--;
+                total += (maxEnd - minEnd + 1);
+            }
+        }
+        return total;
     }
     int helper(int[] nums, int k) {
         int i = 0, j = 0, ans = 0;
