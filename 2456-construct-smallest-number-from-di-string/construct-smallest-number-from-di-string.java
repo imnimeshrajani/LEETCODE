d79a7494c224558746c10ac935dd23d5866c81d5
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-03-30 18:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (58.37%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,25 +1,52 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        StringBuilder ans = new StringBuilder();
-        for(int i = 1; i <= pattern.length() + 1; i++)
-            ans.append(i);
-        int i = 0, j = 0;
-        while (j < pattern.length()) {
-            if(pattern.charAt(j) == 'D') {
-                i = j;
-                while(j < pattern.length() && pattern.charAt(j) == 'D') j++;
-            int temp = j;
-            while(i < temp) {
-                char s = ans.charAt(temp), e = ans.charAt(i);
-                ans.setCharAt(i++, s);
-                ans.setCharAt(temp--, e);
-            }
-            }
-            else 
-                j++;
-            
+        int size = pattern.length();
+        char arr[] =  new char[size+1];
+        for(int i=0;i<=size;i++){
+            arr[i] = Character.forDigit(i+1, 10);
         }
-        System.out.println(ans.toString());
-        return ans.toString();
+        for(int i =0;i<size;i++){
+            int temp = i;
+            while(temp < size && pattern.charAt(temp) == 'D'){
+                temp++;   
+            }
+            reverse(arr, i , temp);
+            if(i!= temp ) i = temp-1;
+
+        }
+
+        return new String(arr);
     }
+    public void reverse(char arr[] , int i  , int j){
+            while(i < j){
+                char temp = arr[i];
+                arr[i]= arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }  
+
+    }
+    //     StringBuilder ans = new StringBuilder();
+    //     for(int i = 1; i <= pattern.length() + 1; i++)
+    //         ans.append(i);
+    //     int i = 0, j = 0;
+    //     while (j < pattern.length()) {
+    //         if(pattern.charAt(j) == 'D') {
+    //             i = j;
+    //             while(j < pattern.length() && pattern.charAt(j) == 'D') j++;
+    //         int temp = j;
+    //         while(i < temp) {
+    //             char s = ans.charAt(temp), e = ans.charAt(i);
+    //             ans.setCharAt(i++, s);
+    //             ans.setCharAt(temp--, e);
+    //         }
+    //         }
+    //         else 
+    //             j++;
+            
+    //     }
+    //     System.out.println(ans.toString());
+    //     return ans.toString();
+    // }
 }
