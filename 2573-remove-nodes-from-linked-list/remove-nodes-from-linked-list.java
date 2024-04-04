aeda37f9c4d2e1cc482a3b5ea0fd5fff1cf4249f
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-04 10:11:06 +0530

    Time: 5 ms (98.20%) | Memory: 61.9 MB (89.30%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -10,26 +10,42 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode ans = new ListNode(0), dummy = ans;
-        while(head != null) {
-            if(dummy.val < head.val) {
-                if(ans.val < head.val){
-                    ans = new ListNode(head.val);
-                    dummy = ans;
-                } else {
-                    ListNode temp = ans;
-                    while(temp.val >= head.val) temp = temp.next;
-                    temp.val = head.val;
-                    temp.next = null;
-                    dummy = temp;
-                }
-            } else {
-                
-                dummy.next = new ListNode(head.val);
-                dummy = dummy.next;
-            }
-            head = head.next;
+        ListNode tail = null, temp;
+        while (head != null) {
+            temp = head.next;
+            head.next = tail;
+            tail = head;
+            head = temp;
         }
-        return ans;
+        int max = Integer.MIN_VALUE;
+        while (tail != null) {
+            if (tail.val >= max) {
+                max = tail.val;
+                head = new ListNode(max, head);
+            }
+            tail = tail.next;
+        }
+        return head;
+        // ListNode ans = new ListNode(0), dummy = ans;
+        // while(head != null) {
+        //     if(dummy.val < head.val) {
+        //         if(ans.val < head.val){
+        //             ans = new ListNode(head.val);
+        //             dummy = ans;
+        //         } else {
+        //             ListNode temp = ans;
+        //             while(temp.val >= head.val) temp = temp.next;
+        //             temp.val = head.val;
+        //             temp.next = null;
+        //             dummy = temp;
+        //         }
+        //     } else {
+                
+        //         dummy.next = new ListNode(head.val);
+        //         dummy = dummy.next;
+        //     }
+        //     head = head.next;
+        // }
+        // return ans;
     }
 }
