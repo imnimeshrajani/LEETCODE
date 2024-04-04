cee43b89fda1f6ca3d2e1fd6e470da2be23e33f1
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-04 11:05:07 +0530

    Time: 2 ms (100.00%) | Memory: 46.8 MB (34.15%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -10,27 +10,39 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        ListNode l1 = reverse(head), l2  = l1;
+        // ListNode l1 = reverse(head), l2  = l1;
         
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0; 
-        while(l1 != null || l2 != null || carry == 1){
-            int sum = 0;
-            if(l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            sum += carry;
-            carry = sum/10;
-            ListNode node = new ListNode(sum % 10);
-            curr = curr.next = node;
+        // ListNode dummy = new ListNode(0);
+        // ListNode curr = dummy;
+        // int carry = 0; 
+        // while(l1 != null || l2 != null || carry == 1){
+        //     int sum = 0;
+        //     if(l1 != null){
+        //         sum += l1.val;
+        //         l1 = l1.next;
+        //     }
+        //     if(l2 != null){
+        //         sum += l2.val;
+        //         l2 = l2.next;
+        //     }
+        //     sum += carry;
+        //     carry = sum/10;
+        //     ListNode node = new ListNode(sum % 10);
+        //     curr = curr.next = node;
+        // }
+        // return reverse(dummy.next);
+        if(head.val > 4){
+            head = new ListNode(0,head);
         }
-        return reverse(dummy.next);
+        ListNode temp = head;
+        while(temp!= null){
+            temp.val = (temp.val*2)%10;
+            if(temp.next != null && temp.next.val > 4){
+                temp.val++;
+            }
+            temp = temp.next;
+        }
+        return head;
     }
 
     ListNode reverse (ListNode head) {
