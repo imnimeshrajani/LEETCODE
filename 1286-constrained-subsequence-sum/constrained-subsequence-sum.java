0cba606a743aba32ee89dbfaaf53de018bccbbb1
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-05 12:31:54 +0530

    Time: 11 ms (99.65%) | Memory: 60.5 MB (13.31%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -1,21 +1,19 @@
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
-        int[] dq = new int[nums.length];
-        int l = 0, r = 1;
-        dq[0] = nums[0];
+        int[] dp = new int[nums.length];
+        int l = 0, r = 1, ans = Integer.MIN_VALUE;
+        dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            nums[i] = Math.max(dq[l] + nums[i], nums[i]);
-            while (r > l && dq[r - 1] < nums[i])
+            nums[i] = Math.max(dp[l] + nums[i], nums[i]);
+            while (r > l && dp[r - 1] < nums[i])
                 r--;
 
-            dq[r] = nums[i];
-            r++;
-            if (i >= k && dq[l] == nums[i - k])
+            dp[r++] = nums[i];
+            if (i >= k && dp[l] == nums[i - k])
                 l++;
         }
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++)
-            ans = Math.max(ans, nums[i]);
+        for (int i : nums)
+            ans = Math.max(ans, i);
 
         return ans;
     }
