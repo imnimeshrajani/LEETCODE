38571dee65d195644f894d390a3b58815db9ce29
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-07 15:14:47 +0530

    Time: 4 ms (96.13%) | Memory: 44.5 MB (88.16%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,25 +1,64 @@
-class MinStack {
-    Stack<int[]> stack = new Stack();
-    int minValue = Integer.MAX_VALUE; 
+// class MinStack {
+//     Stack<int[]> stack = new Stack();
+//     int minValue = Integer.MAX_VALUE; 
 
-    public MinStack() {}
+//     public MinStack() {}
+    
+//     public void push(int val) {
+//         minValue = Math.min(minValue, val);
+//         stack.push(new int[]{val, minValue});
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//         minValue = stack.isEmpty() ? Integer.MAX_VALUE : stack.peek()[1];
+//     }
+    
+//     public int top() {
+//         return stack.peek()[0];
+//     }
+    
+//     public int getMin() {
+//         return minValue;
+//     }
+// }
+
+class MinStack {
+
+    private class Node{
+        public int val;
+        public int min;
+        public Node next;
+
+        public Node(int val, int min, Node next){
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+
+    }
+
+    Node head = null;
+
+    public MinStack() {
+
+    }
     
     public void push(int val) {
-        minValue = Math.min(minValue, val);
-        stack.push(new int[]{val, minValue});
+            head = new Node(val, (head == null) ? val : Math.min(head.min, val), (head == null) ? null : head);
+        
     }
     
     public void pop() {
-        stack.pop();
-        minValue = stack.isEmpty() ? Integer.MAX_VALUE : stack.peek()[1];
+        head = head.next;
     }
     
     public int top() {
-        return stack.peek()[0];
+        return head.val;
     }
     
     public int getMin() {
-        return minValue;
+        return head.min;
     }
 }
 
