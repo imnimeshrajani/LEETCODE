d1c5185c054cb1ec3bdad89883a0f43454ce20c3
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-08 11:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (22.15%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,20 +1,41 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> sandwichesQ = new LinkedList<>(), studentsQ = new LinkedList<>();
-        for(int i : students) studentsQ.add(i);
-        for(int i : sandwiches) sandwichesQ.add(i);
-        int count = 0;
-        while (!sandwichesQ.isEmpty()) {
-            int a = sandwichesQ.peek(), b = studentsQ.poll();
-            if(a != b) {
-                studentsQ.add(b);
-                count++;
-                if(count == studentsQ.size()) break;
+        // Queue<Integer> sandwichesQ = new LinkedList<>(), studentsQ = new LinkedList<>();
+        // for(int i : students) studentsQ.add(i);
+        // for(int i : sandwiches) sandwichesQ.add(i);
+        // int count = 0;
+        // while (!sandwichesQ.isEmpty()) {
+        //     int a = sandwichesQ.peek(), b = studentsQ.poll();
+        //     if(a != b) {
+        //         studentsQ.add(b);
+        //         count++;
+        //         if(count == studentsQ.size()) break;
+        //     } else {
+        //         sandwichesQ.poll();
+        //         count = 0;
+        //     }
+        // }
+        // return studentsQ.size();
+
+        int CSW = 0, SSW = 0;
+
+        for (int student : students){
+            if (student == 1){
+                SSW++;
             } else {
-                sandwichesQ.poll();
-                count = 0;
+                CSW++;
             }
         }
-        return studentsQ.size();
+
+        for (int sandwiche : sandwiches){
+            if (sandwiche == 1 && SSW > 0){
+                SSW--;
+            } else if (sandwiche == 0 && CSW > 0){
+                CSW--;
+            } else {
+                return SSW + CSW;
+            }
+        }
+        return 0;
     }
 }
