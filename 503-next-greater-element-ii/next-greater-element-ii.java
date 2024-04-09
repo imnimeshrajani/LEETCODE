d1aaace69f31ebaf77a8143ae281042edbc3b444
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-09 12:13:50 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (23.25%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,16 +1,33 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] temp = new int[nums.length];
-        Stack<Integer> stack = new Stack();
-        for (int i = nums.length - 1; i >= 0; i--) 
-            stack.push(nums[i]);
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int n = nums[i];
-            while(!stack.isEmpty() && stack.peek() <= n)
-                stack.pop();
-            nums[i] = stack.isEmpty() ? - 1 : stack.peek();
-            stack.push(n);
+        // int[] temp = new int[nums.length];
+        // Stack<Integer> stack = new Stack();
+        // for (int i = nums.length - 1; i >= 0; i--) 
+        //     stack.push(nums[i]);
+        // for (int i = nums.length - 1; i >= 0; i--) {
+        //     int n = nums[i];
+        //     while(!stack.isEmpty() && stack.peek() <= n)
+        //         stack.pop();
+        //     nums[i] = stack.isEmpty() ? - 1 : stack.peek();
+        //     stack.push(n);
+        // }
+        // return nums;
+
+        int stack[] = new int[nums.length*2];
+        int top = 0;
+
+        for(int i = nums.length-1; i >= 0; i--){
+            stack[top++] = nums[i];
         }
-        return nums;
+        int ans[] = new int[nums.length];
+        for(int i = nums.length - 1; i >= 0; i--){
+            while(top != 0 && stack[top-1] <= nums[i])
+                top--;
+            
+            ans[i] = (top != 0) ? stack[top-1] : -1;
+            stack[top++] = nums[i];
+        }
+
+        return ans;
     }
 }
