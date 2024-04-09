f4cf6b2ada43613c84f42ce7cdbf4941b8dc6a91
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-09 17:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (92.17%) - LeetSync

--- 2386-min-max-game/min-max-game.java
+++ 2386-min-max-game/min-max-game.java
@@ -7,23 +7,15 @@ class Solution {
     // return nums[0];
         int[] arr = new int[nums.length/2];
         if(nums.length == 1) 
-        {
             return nums[0];   
-        }
-        for(int i=0;i<nums.length/2;i++)
-        {
+        
+        for(int i=0;i<nums.length/2;i++) {
             if(i % 2 == 0)
-            {
                 arr[i] = Math.min(nums[2*i], nums[2*i+1]);
-            } 
             else if(i % 2 == 1)
-            {
                 arr[i] = Math.max(nums[2*i], nums[2*i+1]);
-            } 
-            else 
-            {
-                return nums[0];
-            }
+            // else 
+            //     return nums[0];
         }
         return minMaxGame(arr);
     }
