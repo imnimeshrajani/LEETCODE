b6c018c8cc2df5aa254420838a8713727b1a80f2
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-10 10:49:32 +0530

    Time: 4 ms (86.41%) | Memory: 42.7 MB (99.03%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,15 +1,32 @@
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
+        // Arrays.sort(deck);
+        // int ans[] = new int[deck.length], index = 0;
+        // Queue<Integer> q  = new LinkedList();
+        // for(int i = 0; i < deck.length; i++) 
+        //     q.add(i);
+        // while(q.size() > 1) {
+        //     ans[q.poll()] = deck[index++];
+        //     q.add(q.poll());
+        // }
+        // ans[q.poll()] = deck[index];
+        // return ans;
         Arrays.sort(deck);
-        int ans[] = new int[deck.length], index = 0;
-        Queue<Integer> q  = new LinkedList();
-        for(int i = 0; i < deck.length; i++) 
-            q.add(i);
-        while(q.size() > 1) {
-            ans[q.poll()] = deck[index++];
-            q.add(q.poll());
+        int[] ans = new int[deck.length];
+        int k = 1, c = 0;
+        ans[0] = deck[0];
+
+        while(k < deck.length){
+            for(int i = 1; i < deck.length; i++){
+                if(ans[i] == 0){
+                    c++;
+                    if(c == 2){
+                        ans[i] = deck[k++];
+                        c = 0;
+                    }
+                }
+            }
         }
-        ans[q.poll()] = deck[index];
         return ans;
     }
 }
