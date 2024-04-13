6326e96c90945cff677f7f3d1517771b0b2fd01d
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-13 12:30:29 +0530

    Time: 11 ms (92.30%) | Memory: 56 MB (96.55%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,35 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int ans = 0;
-        List<Integer> ps = prevSmallerElements(heights), ns = nextSmallerElements(heights);
+        int[] ps = prevSmallerElements(heights), ns = nextSmallerElements(heights);
         for (int i = 0; i < heights.length; i++) {
-            ans = Math.max(ans,(ns.get(i) - ps.get(i) - 1) * heights[i]);
+            // System.out.println("ps: " + ps[i] + " ns: " + ns[i]);
+            ans = Math.max(ans,(ns[i] - ps[i] - 1) * heights[i]);
         }
         return ans;
     }
-    public List<Integer> prevSmallerElements(int[] h) {
-        Stack<Integer> st = new Stack();
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < h.length; i++) {
-            while(!st.isEmpty() && h[st.peek()] >= h[i]) {
-                st.pop();
+    int[] prevSmallerElements(int[] h) {
+        int[] arr = new int[h.length];
+        arr[0] = -1;
+        for(int i = 1; i < h.length; i++) {
+            int idx = i - 1;
+            while(idx >= 0 && h[idx] >= h[i]) {
+                idx = arr[idx];
             }
-            ans.add(st.isEmpty() ? -1 : st.peek());
-            st.push(i);
+            arr[i] = idx;
         }
-        return ans;
+        return arr;
     }
-    public List<Integer> nextSmallerElements(int[] h) {
-        Stack<Integer> st = new Stack();
-        List<Integer> ans = new ArrayList<>();
-        for(int i = h.length - 1; i >= 0; i--) {
-            while(!st.isEmpty() && h[st.peek()] >= h[i]) {
-                st.pop();
+    int[] nextSmallerElements(int[] h) {
+        int[] arr = new int[h.length];
+        arr[arr.length-1] = h.length;
+        for(int i = h.length - 2; i >= 0; i--) {
+            int idx = i + 1;
+            while(idx < h.length && h[idx] >= h[i]) {
+                idx = arr[idx];
             }
-            ans.add(0, st.isEmpty() ? h.length : st.peek());
-            st.push(i);
+            arr[i] = idx;
         }
-        return ans;
+        return arr;
     }
 }
