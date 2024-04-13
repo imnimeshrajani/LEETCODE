b56a827798692eeb253f8746150811f7710fd0e3
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-13 12:59:43 +0530

    Time: 4 ms (98.32%) | Memory: 46.4 MB (48.48%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -9,22 +9,19 @@ class Solution {
                 else 
                     h[j] = 0;
             }
-            ans = Math.max(ans, largestRectangleArea(h));
+            ans = Math.max(ans, nextSmallerElements(h, prevSmallerElements(h)));
         }
         return ans;
     }
-    public int largestRectangleArea(int[] heights) {
-        
-        return nextSmallerElements(heights, prevSmallerElements(heights));
-    }
+    
     int[] prevSmallerElements(int[] h) {
         int[] arr = new int[h.length];
         arr[0] = -1;
         for(int i = 1; i < h.length; i++) {
             int idx = i - 1;
-            while(idx >= 0 && h[idx] >= h[i]) {
+            while(idx >= 0 && h[idx] >= h[i]) 
                 idx = arr[idx];
-            }
+            
             arr[i] = idx;
         }
         return arr;
@@ -37,13 +34,12 @@ class Solution {
         ans = Math.max(ans,(arr[n - 1] - ps[n - 1] - 1) * h[n-1]);
         for(int i = n - 2; i >= 0; i--) {
             int idx = i + 1;
-            while(idx < n && h[idx] >= h[i]) {
+            while(idx < n && h[idx] >= h[i])
                 idx = arr[idx];
-            }
+
             arr[i] = idx;
             ans = Math.max(ans,(arr[i] - ps[i] - 1) * h[i]);
         }
-
         return ans;
     }
 }
