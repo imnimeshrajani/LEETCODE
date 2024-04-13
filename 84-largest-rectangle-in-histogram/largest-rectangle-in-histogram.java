0831e0296954a779acff8fc1d00a08c2fb5ab895
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-13 12:06:51 +0530

    Time: 2183 ms (5.04%) | Memory: 60.5 MB (9.82%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,18 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        if (heights.length == 1) return heights[0];
-        Deque<Integer> stack = new ArrayDeque<>();
-        int maxArea = 0;
-
-        for (int i = 0; i <= heights.length; i++) {
-            while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] >= heights[i])) {
-                int h = heights[stack.pop()];
-                int left = stack.isEmpty() ? -1 : stack.peek();
-                maxArea = Math.max(maxArea, (i - 1 - left) * h);
-            }
-            stack.push(i);
+        int ans = 0;
+        List<Integer> ps = prevSmallerElements(heights), ns = nextSmallerElements(heights);
+        for (int i = 0; i < heights.length; i++) {
+            ans = Math.max(ans,(ns.get(i) - ps.get(i) - 1) * heights[i]);
         }
-
-        return maxArea;
+        return ans;
+    }
+    public List<Integer> prevSmallerElements(int[] h) {
+        Stack<Integer> st = new Stack();
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < h.length; i++) {
+            while(!st.isEmpty() && h[st.peek()] >= h[i]) {
+                st.pop();
+            }
+            ans.add(st.isEmpty() ? -1 : st.peek());
+            st.push(i);
+        }
+        return ans;
+    }
+    public List<Integer> nextSmallerElements(int[] h) {
+        Stack<Integer> st = new Stack();
+        List<Integer> ans = new ArrayList<>();
+        for(int i = h.length - 1; i >= 0; i--) {
+            while(!st.isEmpty() && h[st.peek()] >= h[i]) {
+                st.pop();
+            }
+            ans.add(0, st.isEmpty() ? h.length : st.peek());
+            st.push(i);
+        }
+        return ans;
     }
 }
