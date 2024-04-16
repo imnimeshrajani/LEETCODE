498d8500d96d06cd114df22efc381dda6fbcdfdc
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-16 10:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (92.46%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -18,15 +18,15 @@ class Solution {
         if(depth == 1) {
             return new TreeNode(val, root, null);
         }
-        helper(root, val, --depth);
+        helper(root, val, depth);
         return root;
     }
 
     void helper(TreeNode root, int val, int level) {
-        if(level == 1) {
+        if(level == 2) {
             root.left = new TreeNode(val, root.left, null);
             root.right = new TreeNode(val, null, root.right);
-        } else if(level > 1) {
+        } else if(level > 2) {
             if(root.left != null) helper(root.left, val, level-1);
             if(root.right != null) helper(root.right, val, level-1);
         }
