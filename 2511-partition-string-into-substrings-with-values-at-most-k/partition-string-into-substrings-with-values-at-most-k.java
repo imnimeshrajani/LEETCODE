d1674bae8780e3df51fbe4d39fd6f770faf6229f
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-17 18:01:17 +0530

    Time: 4 ms (93.86%) | Memory: 44.1 MB (97.37%) - LeetSync

--- 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.java
+++ 2511-partition-string-into-substrings-with-values-at-most-k/partition-string-into-substrings-with-values-at-most-k.java
@@ -1,12 +1,32 @@
 class Solution {
     public int minimumPartition(String s, int k) {
-        int ans = 0, left = 0, right = 0;
-        while(right < s.length()) {
-            while(right < s.length() && Long.parseLong(s.substring(left,right + 1)) <= k) right++;
-            ans++;
-            if(left == right) return -1;
-            left = right;
+        // int ans = 0, left = 0, right = 0;
+        // while(right < s.length()) {
+        //     while(right < s.length() && Long.parseLong(s.substring(left,right + 1)) <= k) right++;
+        //     ans++;
+        //     if(left == right) return -1;
+        //     left = right;
+        // }
+        // return ans;
+
+        int n = s.length();
+        int[] str = new int[n];
+        for(int i=0; i<n; i++)
+            str[i] = s.charAt(i)-'0';
+        
+        int p = 0;
+        for(int i = 0; i < n; i++){
+            long temp  = str[i]; 
+            if(temp > k) return -1;
+            int j = i + 1;
+            while(j<n&&temp<=(long)k)
+                temp = temp*(long)10+(long)str[j++];
+
+            p++;
+            if(temp <= (long)k) return p;
+            i = (j == i + 1) ? j - 1 : j - 2;
+            
         }
-        return ans;
+        return p;
     }
 }
