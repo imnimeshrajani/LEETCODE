e0eec70bffc86124ff1b38782ac457568fe8886c
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-18 09:43:15 +0530

    Time: 5 ms (51.43%) | Memory: 46.4 MB (5.11%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,6 +1,6 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-            int count = 0;
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1){
