b4b8e26b89724f4bca5eac5dd1d02c9e0b9e9488
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-24 13:56:39 +0530

    Time: 1 ms (100.00%) | Memory: 41.4 MB (13.05%) - LeetSync

--- 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.java
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.java
@@ -2,17 +2,17 @@ class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
         if(numerator == 0) return "0";
         StringBuilder ans = new StringBuilder();
-        ans.append(((numerator>0) ^ (denominator>0)) ? "-" : "");
+        ans.append(((numerator > 0) != (denominator > 0)) ? "-" : "");
 
-        long num = Math.abs((long)numerator), deno = Math.abs((long)denominator);
-        long afterDiv = num/deno, remains = num%deno;
+        long num = Math.abs((long) numerator), deno = Math.abs((long) denominator);
+        long afterDiv = num / deno, remains = num % deno;
         ans.append(afterDiv);
 
-        if(remains==0) return ans.toString();
+        if(remains == 0) return ans.toString();
         else {
             ans.append(".");
             HashMap<Long, Integer> map = new HashMap<>();
-            while(remains!=0){
+            while(remains != 0){
                 if(map.containsKey(remains)) {
                     ans.insert(map.get(remains), "(");
                     ans.append(")");
@@ -21,8 +21,8 @@ class Solution {
                 else {
                     map.put(remains, ans.length());
                     remains *= 10;
-                    afterDiv = remains/deno;
-                    remains = remains%deno;
+                    afterDiv = remains / deno;
+                    remains = remains % deno;
                     ans.append(afterDiv);
                 }
             }
