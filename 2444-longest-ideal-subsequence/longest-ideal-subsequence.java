344386c91aa4e30233724e6595dd3a420cc42b31
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-25 10:16:00 +0530

    Time: 14 ms (99.21%) | Memory: 44.7 MB (62.70%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
@@ -1,17 +1,41 @@
 class Solution {
     public int longestIdealString(String s, int k) {
-        int[] dp = new int[26];
-        int ans = 0;
-        for(char c : s.toCharArray()) {
-            int val = c - 'a';
-            for(int j = val; j >= 0 && j >= val - k; j--){
-                dp[val] = Math.max(dp[val], dp[j] + 1);
+        // int[] dp = new int[26];
+        // int ans = 0;
+        // for(char c : s.toCharArray()) {
+        //     int val = c - 'a';
+        //     for(int j = val; j >= 0 && j >= val - k; j--){
+        //         dp[val] = Math.max(dp[val], dp[j] + 1);
+        //     }
+        //     for(int j = val + 1; j < 26 && j <= val + k; j++){
+        //         dp[val] = Math.max(dp[val], dp[j] + 1);
+        //     }
+        //     ans = Math.max(ans,dp[val]);
+        // }
+        // return ans;
+
+        int[] dp = new int[27];
+        int n = s.length();
+        
+        for(int i = n-1; i >= 0 ;i--){
+            char cc = s.charAt(i);
+            int idx = cc - 'a';
+            int max  = Integer.MIN_VALUE;
+            
+            int left = Math.max((idx-k), 0);
+            int right = Math.min((idx + k), 26);
+            
+            for(int j = left; j <= right ; j++){
+                max = Math.max(max, dp[j]);
             }
-            for(int j = val + 1; j < 26 && j <= val + k; j++){
-                dp[val] = Math.max(dp[val], dp[j] + 1);
-            }
-            ans = Math.max(ans,dp[val]);
+            
+            dp[idx] = max+1;
         }
-        return ans;
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i : dp) max = Math.max(i, max);
+        
+        return max;
     }
 }
