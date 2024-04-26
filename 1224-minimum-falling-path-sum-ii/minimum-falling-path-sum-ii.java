0d72fd16cc733cbb9840e80d2abf6eed35f3755a
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-04-26 10:51:38 +0530

    Time: 5 ms (87.07%) | Memory: 49.7 MB (75.85%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,16 +1,49 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-        for(int i = 1; i < grid.length; i++) 
-            for(int j = 0; j < grid.length; j++) 
-                    grid[i][j] += helper(grid[i-1],j);
-            
-        return helper(grid[grid.length - 1], Integer.MAX_VALUE);
-    }
-    int helper(int[] arr,int j){
-        int min = Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(i!=j) min = Math.min(min,arr[i]);
+        if(grid.length == 1) return grid[0][0];
+        int firstMin = 0, secMin = 1;
+        for(int i = 0; i < grid.length; i++) {
+            if(grid[0][i] < grid[0][firstMin]) {
+                secMin = firstMin;
+                firstMin = i;
+            } else if(i != firstMin && grid[0][i] < grid[0][secMin]) 
+                secMin  = i;
         }
-        return min;
+        for(int i = 1; i < grid.length; i++) {
+            int min1 = 0, min2 = 1;
+            for(int j = 0; j < grid.length; j++) {
+                grid[i][j] += (firstMin == j) ? grid[i-1][secMin] : grid[i-1][firstMin];
+                
+                if(grid[i][j] < grid[i][min1]) {
+                    min2 = min1;
+                    min1 = j;
+                } else if(j != min1 && grid[i][j] < grid[i][min2]) 
+                    min2  = j;
+
+            }
+            firstMin = min1;
+            secMin = min2;
+        }
+        return grid[grid.length - 1][firstMin];
     }
+
+
+
+
+
+
+    // public int minFallingPathSum(int[][] grid) {
+    //     for(int i = 1; i < grid.length; i++) 
+    //         for(int j = 0; j < grid.length; j++) 
+    //                 grid[i][j] += helper(grid[i-1],j);
+            
+    //     return helper(grid[grid.length - 1], Integer.MAX_VALUE);
+    // }
+    // int helper(int[] arr,int j){
+    //     int min = Integer.MAX_VALUE;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(i!=j) min = Math.min(min,arr[i]);
+    //     }
+    //     return min;
+    // }
 }
