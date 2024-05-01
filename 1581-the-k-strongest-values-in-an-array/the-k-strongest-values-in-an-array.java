3c94800dbdd5ba10c900014e5e22091167741de2
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-01 14:28:48 +0530

    Time: 34 ms (63.04%) | Memory: 61.9 MB (52.72%) - LeetSync

--- 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.java
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.java
@@ -3,11 +3,10 @@ class Solution {
         Arrays.sort(arr);
         int start = 0, end = arr.length - 1, mid = end / 2, idx = 0, ans[] = new int[k];
         while(idx < k) {
-            if(Math.abs(arr[start] - arr[mid]) == Math.abs(arr[end] - arr[mid])) {
-                ans[idx++] = (arr[start] > arr[end]) ? arr[start++] : arr[end--];
-            } else {
-                ans[idx++] = (Math.abs(arr[start] - arr[mid]) > Math.abs(arr[end] - arr[mid])) ? arr[start++] : arr[end--];
-            }
+            ans[idx++] = (Math.abs(arr[start] - arr[mid]) == Math.abs(arr[end] - arr[mid])) ?
+                (arr[start] > arr[end]) ? arr[start++] : arr[end--] :
+                (Math.abs(arr[start] - arr[mid]) > Math.abs(arr[end] - arr[mid])) ? arr[start++] : arr[end--];
+            
         }
         return ans;
     }
