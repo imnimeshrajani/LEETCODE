0d7b3f9fb0a8ad17e9441029a5484c812b84a7e8
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-01 13:49:16 +0530

    Time: 5 ms (82.95%) | Memory: 68.1 MB (29.15%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,14 +1,31 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int pivotCount = 0, smallerCount = 0, ans[] = new int[nums.length], start = 0;
-        for (int i :  nums) 
-            if(i == pivot) pivotCount++;
-            else if(i < pivot) smallerCount++;
-        while (pivotCount-- > 0) 
-            ans[smallerCount++] = pivot;
-        for(int i : nums)
-            if(i > pivot)  ans[smallerCount++] = i;
-            else if(i < pivot) ans[start++] = i;
+        // int pivotCount = 0, smallerCount = 0, ans[] = new int[nums.length], start = 0;
+        // for (int i :  nums) 
+        //     if(i == pivot) pivotCount++;
+        //     else if(i < pivot) smallerCount++;
+        // while (pivotCount-- > 0) 
+        //     ans[smallerCount++] = pivot;
+        // for(int i : nums)
+        //     if(i > pivot)  ans[smallerCount++] = i;
+        //     else if(i < pivot) ans[start++] = i;
+        // return ans;
+
+        int ans [] = new int[nums.length], j = 0;
+
+        for(int val: nums) 
+            if(val < pivot)
+                ans[j++] = val;
+        
+        for(int val : nums)
+            if(val == pivot)
+                ans[j++] = val;
+
+        for(int val : nums)
+            if(val > pivot)
+                ans[j++] = val;
+                j++;
+
         return ans;
     }
 }
