bfd479fcae74a5aec4f533c16d2b9939eb569131
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-01 14:30:47 +0530

    Time: 34 ms (63.04%) | Memory: 61.8 MB (52.72%) - LeetSync

--- 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.java
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] getStrongest(int[] arr, int k) {
         Arrays.sort(arr);
-        int start = 0, end = arr.length - 1, mid = end / 2, idx = 0, ans[] = new int[k];
+        int start = 0, end = arr.length - 1, mid = arr[end / 2], idx = 0, ans[] = new int[k];
         while(idx < k)
-            ans[idx++] = (Math.abs(arr[start] - arr[mid]) == Math.abs(arr[end] - arr[mid])) ? (arr[start] > arr[end]) ? arr[start++] : arr[end--] : (Math.abs(arr[start] - arr[mid]) > Math.abs(arr[end] - arr[mid])) ? arr[start++] : arr[end--];
+            ans[idx++] = (Math.abs(arr[start] - mid) == Math.abs(arr[end] - mid)) ? (arr[start] > arr[end]) ? arr[start++] : arr[end--] : (Math.abs(arr[start] - mid) > Math.abs(arr[end] - mid)) ? arr[start++] : arr[end--];
             
         return ans;
     }
