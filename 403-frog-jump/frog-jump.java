64e4c0b8d58fcf5a4be951042b02831c79844d24
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-02 11:26:53 +0530

    Time: 2 ms (100.00%) | Memory: 44.2 MB (99.69%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,23 +1,56 @@
-class Solution {
-    public boolean canCross(int[] stones) {
-        int n = stones.length;
-        int stonesFarthest = (n * (n + 1)) / 2;
-        if (stones[1] != 1 || stones[n - 1] - stones[0] > stonesFarthest) return false;
+// class Solution {
+//     public boolean canCross(int[] stones) {
+//         int n = stones.length;
+//         int stonesFarthest = (n * (n + 1)) / 2;
+//         if (stones[1] != 1 || stones[n - 1] - stones[0] > stonesFarthest) return false;
 
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for(int i : stones) map.put(i, new HashSet<Integer>());
-        map.get(stones[0]).add(1);
-        for(int i : stones) {
-            Set<Integer> set = map.get(i);
-            for(Integer val : set) {
-                if(map.containsKey(i + val)) {
-                    Set<Integer> temp = map.get(i + val);
-                    if(val > 1) temp.add(val - 1);
-                    temp.add(val);
-                    temp.add(val + 1);
+//         Map<Integer, Set<Integer>> map = new HashMap<>();
+//         for(int i : stones) map.put(i, new HashSet<Integer>());
+//         map.get(stones[0]).add(1);
+//         for(int i : stones) {
+//             Set<Integer> set = map.get(i);
+//             for(Integer val : set) {
+//                 if(map.containsKey(i + val)) {
+//                     Set<Integer> temp = map.get(i + val);
+//                     if(val > 1) temp.add(val - 1);
+//                     temp.add(val);
+//                     temp.add(val + 1);
+//                 }
+//             }
+//         }
+//         return !map.get(stones[n - 1]).isEmpty();
+//     }
+// }
+
+
+class Solution {
+    boolean dp[][];
+    public boolean canCross(int[] stones) {
+        int n = stones.length, stonesFarthest = (n * (n + 1)) / 2;
+        if (stones[1] != 1 || stones[n - 1] - stones[0] > stonesFarthest) 
+            return false;
+        
+        dp = new boolean[n][n];
+        return helper(stones, 0, 1);
+    }
+    
+    boolean helper(int[] stones, int lastStone, int currStone) {
+        if (currStone == stones.length - 1) {
+            return true;
+        }
+        if(dp[lastStone][currStone]) return false;
+        int lastJump = stones[currStone] - stones[lastStone];
+        int nextStone = currStone + 1;
+        while (nextStone < stones.length && stones[nextStone] <= stones[currStone] + lastJump + 1) {
+            int nextJump = stones[nextStone] - stones[currStone];
+            if (nextJump >= lastJump - 1 && nextJump <= lastJump + 1) {
+                if (helper(stones, currStone, nextStone)) {
+                    return true;
                 }
             }
+            nextStone++;
         }
-        return !map.get(stones[n - 1]).isEmpty();
+        dp[lastStone][currStone] = true;
+        return false;
     }
 }
