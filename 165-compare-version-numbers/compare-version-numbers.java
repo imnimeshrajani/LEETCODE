6d4c2d54cc8a536f4dd48e0d2f7c349224deaa9d
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-03 11:00:35 +0530

    Time: 1 ms (78.87%) | Memory: 41.3 MB (70.46%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -8,7 +8,7 @@ class Solution {
             int val1 = idx1 <= v1.length ? Integer.parseInt(version1.substring(prev1, idx1++)) : 0;
             while(idx2 < v2.length && v2[idx2] != '.') idx2++;
             int val2 = idx2 <= v2.length ? Integer.parseInt(version2.substring(prev2, idx2++)) : 0;
-            System.out.println(val1 + " " + val2);
+            // System.out.println(val1 + " " + val2);
             if(val1 > val2) return 1;
             if(val1 < val2) return -1;
             if(idx1 >= v1.length && idx2 >= v2.length) break;
