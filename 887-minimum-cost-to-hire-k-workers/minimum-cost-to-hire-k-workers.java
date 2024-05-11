2fa6c2b06849d14c587ef71e1cb697577a0988c9
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-11 10:14:00 +0530

    Time: 23 ms (100.00%) | Memory: 45.7 MB (63.64%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -1,33 +1,66 @@
-class Solution {
-    class Pair {
-        double ratio;
-        int worker;
-        Pair (double ratio, int worker) {
-            this.ratio = ratio;
-            this.worker = worker;
-        }
+// class Solution {
+//     class Pair {
+//         double ratio;
+//         int worker;
+//         Pair (double ratio, int worker) {
+//             this.ratio = ratio;
+//             this.worker = worker;
+//         }
+//     }
+//     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+//         List<Pair> list  = new ArrayList<>();
+//         int qualitySum = 0;
+//         double ans = 0.0, maxRate = 0.0;
+//         for(int i = 0;  i < quality.length; i++) 
+//             list.add(new Pair((double)wage[i]/quality[i], i));
+//         list.sort(Comparator.comparingDouble(value -> value.ratio));
+//         for(int i = 0; i < k; i++) {
+//             qualitySum += quality[list.get(i).worker];
+//             maxRate = Math.max(maxRate, list.get(i).ratio);
+//             pq.add(quality[list.get(i).worker]);
+//         }
+//         ans = maxRate * qualitySum;
+//         for(int i = k; i < quality.length; i++) {
+//             maxRate = Math.max(maxRate, list.get(i).ratio);
+//             qualitySum -= pq.poll();
+//             qualitySum += quality[list.get(i).worker];
+//             pq.add(quality[list.get(i).worker]);
+//             ans = Math.min(ans, maxRate * qualitySum);
+//         }
+//         return ans;
+//     }
+// }
+
+class Worker implements Comparable<Worker> {
+    final int q, w;
+    public Worker(int q, int w) {
+        this.q = q;
+        this.w = w;
     }
+    @Override
+    public int compareTo(Worker other) {
+        return Integer.compare(w * other.q, q * other.w);
+    }
+}
+class Solution {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        List<Pair> list  = new ArrayList<>();
-        int qualitySum = 0;
-        double ans = 0.0, maxRate = 0.0;
-        for(int i = 0;  i < quality.length; i++) 
-            list.add(new Pair((double)wage[i]/quality[i], i));
-        list.sort(Comparator.comparingDouble(value -> value.ratio));
-        for(int i = 0; i < k; i++) {
-            qualitySum += quality[list.get(i).worker];
-            maxRate = Math.max(maxRate, list.get(i).ratio);
-            pq.add(quality[list.get(i).worker]);
+        int n = quality.length;
+        Worker[] a = new Worker[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = new Worker(quality[i], wage[i]);
         }
-        ans = maxRate * qualitySum;
-        for(int i = k; i < quality.length; i++) {
-            maxRate = Math.max(maxRate, list.get(i).ratio);
-            qualitySum -= pq.poll();
-            qualitySum += quality[list.get(i).worker];
-            pq.add(quality[list.get(i).worker]);
-            ans = Math.min(ans, maxRate * qualitySum);
+        Arrays.sort(a);
+
+        int s = 0;
+        double res = 1e15;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (Worker worker: a) {
+            q.add(-worker.q);
+            s += worker.q;
+            if (q.size() > k) s += q.poll();
+            if (q.size() == k) res = Math.min(res, (double) s * worker.w / worker.q);
         }
-        return ans;
+        return res;
     }
 }
