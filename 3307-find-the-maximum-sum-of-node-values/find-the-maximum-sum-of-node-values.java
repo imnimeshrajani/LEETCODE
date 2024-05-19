07cadb635edbfe01dc55034f1d89ef31204dc0bb
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-19 11:02:24 +0530

    Time: 1 ms (100.00%) | Memory: 55 MB (39.69%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
@@ -1,18 +1,36 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        int n = nums.length, xorArray[] = new int[n];;
-        if(n == 1) return nums[0];
+        // int n = nums.length, xorArray[] = new int[n];;
+        // if(n == 1) return nums[0];
 
-        for(int i = 0; i < n; i++)
-            xorArray[i] = (nums[i] ^ k);
+        // for(int i = 0; i < n; i++)
+        //     xorArray[i] = (nums[i] ^ k);
 
-        long option = xorArray[0], ans = nums[0];
-        for(int i = 1; i < n; i++) {
-            long tempOption = Math.max(option + nums[i], ans + xorArray[i]);
-            long tempAns = Math.max(ans + nums[i], option + xorArray[i]);
-            option = tempOption;
-            ans = tempAns;
+        // long option = xorArray[0], ans = nums[0];
+        // for(int i = 1; i < n; i++) {
+        //     long tempOption = Math.max(option + nums[i], ans + xorArray[i]);
+        //     long tempAns = Math.max(ans + nums[i], option + xorArray[i]);
+        //     option = tempOption;
+        //     ans = tempAns;
+        // }
+        // return ans;
+
+
+        long sum = 0, minExtra = 1000000;
+        int count = 0;
+
+        for( int val : nums) {
+            if ((val ^ k) > val ) {
+                sum += val ^ k;
+                minExtra = Math.min(minExtra, (val ^ k)- val);
+                count++;
+            } else {
+                sum += val;
+                minExtra = Math.min(minExtra, val - (val ^ k));
+            }
         }
-        return ans;
+
+        return (count % 2 == 0 ) ? sum : sum - minExtra;
+
     }
 }
