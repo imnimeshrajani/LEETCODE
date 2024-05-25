bc41da3509e49b4abcc8691466565f8a969b7411
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-25 10:57:24 +0530

    Time: 1 ms (94.29%) | Memory: 41.6 MB (68.54%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<String> ans = new ArrayList<>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for(String str : wordDict) set.add(str);
+        helper(s, set, 0, new ArrayList<>());
+        return ans;
+    }
+    void helper(String s, Set<String> set, int idx, List<String> arr) {
+        for(int i = idx + 1; i <= s.length(); i++) {
+            String str = s.substring(idx, i);
+            if(set.contains(str)) {
+                arr.add(str);
+                if(i == s.length()) {
+                    StringBuilder sb = new StringBuilder();
+                    for(String word : arr) {
+                        sb.append(word);
+                        sb.append(" ");
+                    }
+                    sb.deleteCharAt(sb.length() - 1);
+                    ans.add(sb.toString());
+                } else helper(s, set, i, arr);
+                arr.remove(arr.size() - 1);
+            }
+        }
+    }
+}
