1b3967b391dc1e64f68830434f3fae25a48bf43c
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-28 11:03:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (89.99%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,30 +10,64 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        int l = 0;
-    ListNode cur = head;
-    while (cur != null) {
-        l++;
-        cur = cur.next;
-    }
-    if (l < k) 
-        return head;
+    //     int l = 0;
+    // ListNode cur = head;
+    // while (cur != null) {
+    //     l++;
+    //     cur = cur.next;
+    // }
+    // if (l < k) 
+    //     return head;
 
-    int n = l/k;
-    ListNode pre = new ListNode(0), node, rem, ret = pre;
-    for (int i = 0; i < n; i++) {
-        node = null;
-        rem = head;
-        for (int j = 0; j < k; j++) {
-            ListNode nxt = head.next;
-            head.next = node;
-            node = head;
-            head = nxt;
+    // int n = l/k;
+    // ListNode pre = new ListNode(0), node, rem, ret = pre;
+    // for (int i = 0; i < n; i++) {
+    //     node = null;
+    //     rem = head;
+    //     for (int j = 0; j < k; j++) {
+    //         ListNode nxt = head.next;
+    //         head.next = node;
+    //         node = head;
+    //         head = nxt;
+    //     }
+    //     pre.next = node;
+    //     rem.next = head;
+    //     pre = rem;
+    // }
+    // return ret.next;
+    ListNode preHead = new ListNode();
+        preHead.next = head;
+
+        ListNode prev = preHead, cur = head;
+        while (cur != null) {
+            ListNode n1 = cur, n2 = cur;
+            int j = k;
+            while (n2 != null && j > 0) {
+                n1 = n2;
+                n2 = n2.next;
+                j--;
+            }
+            if (j > 0) break;
+
+            n1.next = null;
+            ListNode r = reverse(cur);
+            prev.next = r;
+            cur.next = n2;
+            prev = cur;
+            cur = n2;
         }
-        pre.next = node;
-        rem.next = head;
-        pre = rem;
+
+        return preHead.next;
     }
-    return ret.next;
+
+    ListNode reverse(ListNode cur) {
+        ListNode prev = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
     }
 }
