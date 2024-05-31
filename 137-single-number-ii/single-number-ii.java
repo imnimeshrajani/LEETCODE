b872248a1b51851f342774f3aec7c918bad3a5d7
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-05-31 11:06:41 +0530

    Time: 1 ms (84.88%) | Memory: 45.2 MB (57.80%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -3,9 +3,8 @@ class Solution {
         int ans = 0;
         for(int i = 0; i < 32; i++) {
             int sum = 0;
-            for(int num : nums) {
+            for(int num : nums) 
                 sum += num >> i & 1;
-            }
             sum %= 3;
             ans |= sum << i;
         }
