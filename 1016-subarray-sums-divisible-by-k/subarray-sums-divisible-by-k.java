32ee1ff77aef9a3e60effc96e35b1d9a728582ac
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-06-09 11:19:35 +0530

    Time: 3 ms (99.31%) | Memory: 45.9 MB (85.93%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,15 +1,31 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int ans = 0, sum = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        for(int i  : nums) {
+        // int ans = 0, sum = 0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // map.put(0, 1);
+        // for(int i  : nums) {
+        //     sum += i;
+        //     int rem = sum % k;
+        //     if(rem < 0) rem += k;
+        //     ans += map.getOrDefault(rem, 0);
+        //     map.put(rem, map.getOrDefault(rem, 0) + 1);
+        // }
+        // return ans;
+
+        int count=0,sum=0,rem;
+        int md[]=new int[k];
+        for(int i : nums) {
             sum += i;
-            int rem = sum % k;
-            if(rem < 0) rem += k;
-            ans += map.getOrDefault(rem, 0);
-            map.put(rem, map.getOrDefault(rem, 0) + 1);
+            rem = sum % k;
+            if(rem < 0)
+            rem += k;
+            md[rem]++;
         }
-        return ans;
+        for(int i : md)
+            if(i > 1)
+                count += (i * (i - 1)) /2;
+
+        count+=md[0];
+        return count;
     }
 }
