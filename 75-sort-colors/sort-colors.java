b19202e5abb2b29a8e053ec8e8fc6e52b226fe21
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-06-12 08:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (77.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int R = 0, G = 0, B = 0;
-        for(int i : nums)
-            if(i == 0) R++;
-            else if(i == 1) G++;
-            else if(i == 2) B++;
+        // int R = 0, G = 0, B = 0;
+        // for(int i : nums)
+        //     if(i == 0) R++;
+        //     else if(i == 1) G++;
+        //     else if(i == 2) B++;
         
-        for(int i = 0; i < nums.length; i++) {
-            if(R-- > 0) nums[i] = 0;
-            else if(G-- > 0) nums[i] = 1;
-            else if(B-- > 0) nums[i] = 2;
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(R-- > 0) nums[i] = 0;
+        //     else if(G-- > 0) nums[i] = 1;
+        //     else if(B-- > 0) nums[i] = 2;
+        // }
+
+        int i = 0, start = 0, end = nums.length - 1;
+        while (i <= end) {
+            if(nums[i] == 0) swap(nums, i++, start++);
+            else if(nums[i] == 2) 
+                swap(nums, i, end--);
+            else i++;
+            
         }
+    }
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
