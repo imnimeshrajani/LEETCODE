ed207df5a2afb5d8efeaffc3e4dd85605bafcbb6
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-06-20 11:13:07 +0530

    Time: 35 ms (99.46%) | Memory: 55.8 MB (37.12%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -4,22 +4,21 @@ class Solution {
         int start = 1, end = (position[position.length - 1] - position[0]) / (m - 1), ans = 1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
-            if(canSet(position, mid, m)) {
+            if(canSet(position, mid) >= m) {
                 ans = mid;
                 start = ++mid;
             } else end = --mid;
         }
         return ans;
     }
-    boolean canSet(int[] arr, int distance, int m) {
+    int canSet(int[] arr, int distance) {
         int count = 1, last = arr[0];
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] - last >= distance) {
                 count++;
                 last = arr[i];
             }
-            if(count >= m) return true;
         }
-        return false;
+        return count;
     }
 }
