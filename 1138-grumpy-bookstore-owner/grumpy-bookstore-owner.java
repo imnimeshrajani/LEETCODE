1a5ea47daf7235a10f401deb1f29ea4745c6e793
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-06-21 09:12:12 +0530

    Time: 3 ms (91.49%) | Memory: 45.5 MB (98.26%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -8,13 +8,8 @@ class Solution {
             }
         }
         while(end < n) {
-            if(minutes > 0) {
-                sum += customers[end++];
-                minutes--;
-            } else {
-                sum -= customers[start++];
-                minutes++;
-            }
+            sum = (minutes > 0) ? sum + customers[end++] : sum - customers[start++];
+            minutes += (minutes > 0) ? -1 : 1;
             max = Math.max(max, sum);
         }
         return ans + max;
