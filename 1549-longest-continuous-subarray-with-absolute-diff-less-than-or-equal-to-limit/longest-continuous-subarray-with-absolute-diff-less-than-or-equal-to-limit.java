c7e1ca80272af78fc81fbd348ed77d33b961e5ad
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-06-23 12:09:29 +0530

    Time: 27 ms (91.14%) | Memory: 57.8 MB (18.52%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,16 +1,33 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        int left = 0;
-        TreeSet<Integer> set = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : nums[a] - nums[b]);
-        set.add(0);
-        int res = 1;
-        for (int right = 1; right < nums.length; right++) {
-            set.add(right);
-            while (nums[set.last()] - nums[set.first()] > limit) {
-                set.remove(left++);
+        LinkedList<Integer> increase = new LinkedList<>(), decrease = new LinkedList<>();
+        int max = 0, left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+
+            while (increase.size() > 0 && n < increase.getLast()) 
+                increase.removeLast();
+            
+            increase.add(n);
+
+            while (decrease.size() > 0 && n > decrease.getLast()) 
+                decrease.removeLast();
+            
+            decrease.add(n);
+
+            while (decrease.getFirst() - increase.getFirst() > limit) {
+                if (nums[left] == decrease.getFirst()) 
+                    decrease.removeFirst();
+                
+                if (nums[left] == increase.getFirst()) 
+                    increase.removeFirst();
+                
+                left++;
             }
-            res = Math.max(res, right - left + 1);
+            max = Math.max(max, (i - left + 1));
         }
-        return res;
+
+        return max;
     }
 }
