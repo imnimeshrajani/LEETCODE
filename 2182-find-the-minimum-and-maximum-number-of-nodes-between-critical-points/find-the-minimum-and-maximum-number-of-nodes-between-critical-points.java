eea6a3f5909518baf4f03b95fb4cf6274210bf6f
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-07-05 09:23:47 +0530

    Time: 4 ms (100.00%) | Memory: 60.5 MB (54.76%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,15 +10,14 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] ans = new int[] {-1, -1};
-        if(head.next.next == null) return ans;
+        if(head.next.next == null) return new int[] {-1, -1};
         int prev = head.val, first = 0, last = 0, idx = 1, min = Integer.MAX_VALUE;
         head = head.next;
         while(head.next != null) {
             if((head.val > prev && head.val > head.next.val) || (head.val < prev && head.val < head.next.val)) {
-                if(first == 0) {
+                if(first == 0)
                     first = idx;
-                } else {
+                else {
                     if(last == 0) {
                         min = Math.min(min, idx - first);
                         last = idx;
@@ -32,8 +31,6 @@ class Solution {
             head = head.next;
             idx++;
         }
-        ans[0] = min;
-        ans[1] = last - first;
-        return min == Integer.MAX_VALUE ? new int[] {-1, -1} : ans;
+        return min == Integer.MAX_VALUE ? new int[] {-1, -1} : new int[] {min, last - first};
     }
 }
