264d2d6ce639671ea951f4574b314ac9e98e6885
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-07-08 08:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.17%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return solve(n, k)+1;
+        return solve(n, k) + 1;
     }
     public int solve(int n,int k) {
-        return (n == 1) ? 0 : (solve(n-1,k)+k)%n;
+        return n == 1 ? 0 : (solve(n - 1, k) + k) % n;
     }
 }
