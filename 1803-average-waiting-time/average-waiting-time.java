74b1bd9e57278dce55c13dc511d3ee66fa6cd3df
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-07-09 10:19:41 +0530

    Time: 5 ms (19.18%) | Memory: 72.6 MB (58.37%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,13 +1,10 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        long waiting = 0;
+        double waiting = 0;
         int time = 0;
         for(int[] arr : customers) {       
-            if(time < arr[0]) 
-                time = arr[0];
-            waiting += time + arr[1] - arr[0];    
-            time += arr[1];
-            
+            time = (time < arr[0]) ? arr[0] + arr[1] : time + arr[1];
+            waiting += time - arr[0];    
         }
         return (double) waiting / customers.length;
     }
