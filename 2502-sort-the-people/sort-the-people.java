5429bc446488a752ac604942651d965d0fbfbfc8
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-07-22 11:21:31 +0530

    Time: 6 ms (94.61%) | Memory: 45.8 MB (5.32%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,20 +1,12 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, idx = 0;
-        for(int val : heights) {
-            min = Math.min(min, val);
-            max = Math.max(max, val);
-        }
-        int[] freq = new int[(max - min) + 1];
-        Arrays.fill(freq, -1);
-        for(int i = 0; i < heights.length; i++) 
-            freq[heights[i] - min] = i;
-            
-        String[] ans = new String[names.length];
-        for(int i = freq.length - 1; i >= 0; i--) 
-            if(freq[i] > -1) 
-                ans[idx++] = names[freq[i]];
-            
-        return ans;
+        Map<Integer,String> map = new TreeMap<>();
+        String res[] = new String[names.length];
+        for(int i=0; i<names.length; i++) map.put(heights[i],names[i]);
+        int j = res.length-1;
+        for(Map.Entry<Integer,String> m : map.entrySet())
+            res[j--] = m.getValue();
+
+        return res;
     }
 }
