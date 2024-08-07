6600d5fa8438b6ba5255c4b65d688d29a3e495c0
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-08-07 10:22:35 +0530

    Time: 2 ms (82.11%) | Memory: 41.4 MB (96.58%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -2,10 +2,11 @@ class Solution {
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
         String[] numbers = new String[] {"", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ", "Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
-        return helper(num, 0, new StringBuilder(), numbers).toString().trim();
+        String[] ty = new String[] {"", "", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "};
+        return helper(num, 0, new StringBuilder(), numbers, ty).toString().trim();
     }
-    StringBuilder helper(int num, int stack, StringBuilder ans, String[] numbers) {
-        if(num/1000 != 0) ans = helper(num / 1000, stack + 1, ans, numbers);
+    StringBuilder helper(int num, int stack, StringBuilder ans, String[] numbers, String[] ty) {
+        if(num/1000 != 0) ans = helper(num / 1000, stack + 1, ans, numbers, ty);
         int number = num % 1000;
         // System.out.println("On stack " + stack + " number is : " + number);
         if(number == 0) return ans;
@@ -16,14 +17,10 @@ class Solution {
         } 
         if(number / 10 != 0 && number / 10 > 1) {
             int val = number / 10;
-            if(val == 2) ans.append("Twenty " + numbers[number % 10]);
-            if(val == 3) ans.append("Thirty " + numbers[number % 10]);
-            if(val == 4) ans.append("Forty " + numbers[number % 10]);
-            if(val == 5) ans.append("Fifty " + numbers[number % 10]);
-            if(val == 6) ans.append("Sixty " + numbers[number % 10]);
-            if(val == 7) ans.append("Seventy " + numbers[number % 10]);
-            if(val == 8) ans.append("Eighty " + numbers[number % 10]);
-            if(val == 9) ans.append("Ninety " + numbers[number % 10]);
+            if(val > 1 && val < 10) {
+                ans.append(ty[val]);
+                ans.append(numbers[number % 10]);
+            }
             number %= 10;
         } else {
             ans.append(numbers[number]);
