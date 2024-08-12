4ed8dcfc7d724d0f9452ad600bdc8fa69176b6c0
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-08-12 07:54:12 +0530

    Time: 10 ms (99.98%) | Memory: 47.4 MB (7.72%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,20 +1,68 @@
 class KthLargest {
-    int k;
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    private int size = 0;
+    private final int k;
+    private final int[] heap;
+
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        for(int val : nums) pq.add(val);
+        this.heap = new int[k + 1];
+        for (int num : nums) {
+            add(num);
+        }
     }
-    
-    public int add(int val) {
-        pq.offer(val);
-        while(pq.size() > k) pq.poll();
-        return pq.peek();
-    }
-}
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
+    public int add(int val) {
+        
+        if (size == k) {
+            if (val < heap[1]) {
+                return heap[1];
+            }
+            pop();
+        }
+
+        heap[++size] = val;
+
+        int index = size;
+        int parent = size / 2;
+
+        while (heap[parent] > heap[index] && index > 1) {
+
+            int temp = heap[index];
+            heap[index] = heap[parent];
+            heap[parent] = temp;
+
+            index = parent;
+            parent = parent / 2;
+        }
+
+        return heap[1];
+    }
+
+    public void pop() {
+        heap[1] = heap[size--];
+
+        int index = 1;
+        while (index <= size / 2) {
+            int left = index * 2;
+            int right = (index * 2) + 1;
+
+            if (heap[index] > heap[left] || heap[index] > heap[right]) {
+                if (heap[left] < heap[right]) {
+                    int temp = heap[left];
+                    heap[left] = heap[index];
+                    heap[index] = temp;
+                    index = left;
+                } else {
+                    int temp = heap[right];
+                    heap[right] = heap[index];
+                    heap[index] = temp;
+                    index = right;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+}
