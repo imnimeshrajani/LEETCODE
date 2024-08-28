cf05aff1bedbe206c45990195b245bd18ce203e0
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-08-28 17:57:20 +0530

    Time: 15 ms (99.63%) | Memory: 77.9 MB (78.73%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -1,29 +1,45 @@
 class Solution {
-    int ans = 0;
+    int n, m;
+    int[][] isValid;
+    int[] dx = new int[] {-1, 0 , 0 ,1};
+    int[] dy = new int[] {0, -1, 1, 0};
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int res = 0;
-        for (int i = 0; i < grid1.length; i++) {
-            for (int j = 0; j < grid2[0].length; j++) {
-                if (grid2[i][j] == 1) {
-                    ans = 1;
-                    dfs(grid1, grid2, i, j);
-                    res += ans;
+        n = grid1.length;
+        m = grid1[0].length;
+        isValid = new int[n][m];
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                if (isValid[i][j] == 0 && grid2[i][j] == 1) {
+                    if (isSubIslands(i, j, grid1, grid2)) {
+                        num++;
+                    }
                 }
             }
         }
-        return res;
+        return num;
     }
 
-    
-    public void dfs(int[][] grid1, int[][] grid2, int i, int j) {
-        if (i < 0 || i >= grid1.length || j < 0 || j >= grid1[0].length || grid2[i][j] == 0)
-            return;
-        if (grid1[i][j] == 0)
-            ans = 0;
-        grid2[i][j] = 0;
-        dfs(grid1, grid2, i - 1, j);
-        dfs(grid1, grid2, i + 1, j);
-        dfs(grid1, grid2, i, j + 1);
-        dfs(grid1, grid2, i, j - 1);
+    private boolean isSubIslands(int r, int c, int[][] grid1, int[][] grid2) {
+        if (grid1[r][c] != 1) {
+            isValid[r][c] = -1;
+            return false;
+        }
+        isValid[r][c] = 1;
+        for (int k = 0 ; k < 4; k++) {
+            int nr = r + dx[k];
+            int nc = c + dy[k];
+            if (nr <  0 || nr >= n || nc < 0 || nc >= m) continue;
+            if (isValid[nr][nc] == -1) {
+                isValid[r][c] = -1;
+                return false;
+            }
+            if (isValid[nr][nc] == 1) continue;
+            if (grid2[nr][nc] == 1 && !isSubIslands(nr, nc, grid1, grid2)) {
+                isValid[r][c] = -1;
+                return false;
+            }
+        }
+        return true;
     }
 }
