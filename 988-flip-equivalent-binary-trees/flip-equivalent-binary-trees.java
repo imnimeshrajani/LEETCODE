2e126869631f8fed75545595a9174df2ba3d5081
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-10-24 07:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (41.59%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
@@ -19,8 +19,6 @@ class Solution {
         if(root1 == null || root2 == null) return false;
         if(root1.val != root2.val) return false;
         
-        if (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)) 
-            return true;
-        return false;
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
     }
 }
