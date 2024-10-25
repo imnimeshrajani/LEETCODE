0b80d493591f4d1ec23a68f471bdaed33b47a031
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-10-25 09:53:17 +0530

    Time: 23 ms (99.59%) | Memory: 56.2 MB (40.41%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,14 +1,48 @@
+class Trie{
+    class Node{
+        boolean eow;
+        Node[]children;
+        Node(){
+            children=new Node[27];
+        }
+    }
+    Node root;
+    Trie(){
+        root=new Node();
+    }
+    public void insert(String s){
+        Node curr=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(ch=='/')idx=26;
+            if(curr.children[idx]==null)curr.children[idx]=new Node();
+            curr=curr.children[idx];
+        }
+        curr=curr.children[26]=new Node();
+        curr.eow=true;
+    }
+    public boolean search(String s){
+        Node curr=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(ch=='/')idx=26;
+            if(curr.children[idx]==null)return false;
+            curr=curr.children[idx];
+            if(curr.eow==true)return true;
+        }
+        return false;
+    }
+}
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Arrays.sort(folder);
-        
-        List<String> ans = new ArrayList<>();
-        ans.add(folder[0]);
-        
-        for (int i = 1; i < folder.length; i++) {
-            String lastFolder = ans.get(ans.size() - 1) + "/";
-            if (!folder[i].startsWith(lastFolder)) 
-                ans.add(folder[i]);
+        Trie trie=new Trie();
+        Arrays.sort(folder,(a,b)->a.length()-b.length());
+        List<String>ans=new ArrayList<>();
+        for(String s:folder){
+            if(!trie.search(s)){
+                ans.add(s);
+                trie.insert(s);
+            }
         }
         return ans;
     }
