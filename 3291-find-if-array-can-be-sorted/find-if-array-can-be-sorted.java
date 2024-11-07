2cd341925ce8c89643507a4ed977dd4ad4ffc0ab
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-11-07 15:06:25 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (25.00%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        short pmax = 0, cmin = 0, cmax = 0;
+        int pmax = 0, cmin = 0, cmax = 0;
         byte pcnt = 0;
         for (final int v : nums) {
             final byte ccnt = (byte)Integer.bitCount(v);
             if (pcnt == ccnt) {
-                cmin = cmin > v ? (short)v : cmin;
-                cmax = cmax < v ? (short)v : cmax;
+                cmin = Math.min(cmin, v);
+                cmax = Math.max(cmax, v);
             } else if (cmin < pmax) {
                 return false;
             } else {
                 pmax = cmax;
-                cmin = cmax = (short)v;
+                cmin = cmax = v;
                 pcnt = ccnt;
             }
         }
