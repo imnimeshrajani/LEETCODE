dd91d5a5b46a3ec70b09a9c5da3d9ef4eee409ff
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-11-08 09:07:49 +0530

    Time: 3 ms (78.95%) | Memory: 58.8 MB (61.43%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
@@ -3,11 +3,7 @@ class Solution {
         for(int i = 1; i < nums.length; i++) 
             nums[i] ^= nums[i - 1];
         
-        int ans[] = new int[nums.length], position = ans.length - 1, xor = 2;
-        for(int i = 1; i < maximumBit; i++) 
-            xor *= 2;
-            
-        xor--;
+        int ans[] = new int[nums.length], position = ans.length - 1, xor = (1 << maximumBit) - 1;;
         
         for(int i = 0; i < nums.length; i++) 
             ans[position--] = nums[i] ^ xor;
