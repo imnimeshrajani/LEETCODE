563a98403668c7943c16016d904e16af38358fb6
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-11-14 09:43:52 +0530

    Time: 43 ms (79.64%) | Memory: 56.9 MB (37.70%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
         int start = 1, end = 100000, ans = -1;
         while(start <= end) {
-            int mid = (end + start) / 2;
+            int mid = start + (end - start) / 2;
             if(isPossible(mid, quantities, n)) {
                 ans = mid;
                 end = --mid;
