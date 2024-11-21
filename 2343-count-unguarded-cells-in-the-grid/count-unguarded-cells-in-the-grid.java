c64be01f7d91c272b35e41ef72e7f54b31365bf1
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-11-21 08:42:46 +0530

    Time: 18 ms (100.00%) | Memory: 72.2 MB (30.12%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,36 +1,91 @@
+// class Solution {
+//     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+//         int grid[][] = new int[m][n], ans = 0;
+//         for(int[] wall : walls) 
+//             grid[wall[0]][wall[1]] = 1;
+        
+//         for(int[] guard : guards) 
+//             grid[guard[0]][guard[1]] = 2;
+
+//         for(int[] guard : guards) {
+//             for(int i = guard[1] + 1; i < n; i++) {
+//                 if(grid[guard[0]][i] == 1 || grid[guard[0]][i] == 2) break;
+//                 grid[guard[0]][i] = 3;
+//             }
+//             for(int i = guard[1] - 1; i >= 0; i--) {
+//                 if(grid[guard[0]][i] == 1 || grid[guard[0]][i] == 2) break;
+//                 grid[guard[0]][i] = 3;
+//             }
+//             for(int i = guard[0] + 1; i < m; i++) {
+//                 if(grid[i][guard[1]] == 1 || grid[i][guard[1]] == 2) break;
+//                 grid[i][guard[1]] = 3;
+//             }
+//             for(int i = guard[0] - 1; i >= 0; i--) {
+//                 if(grid[i][guard[1]] == 1 || grid[i][guard[1]] == 2) break;
+//                 grid[i][guard[1]] = 3;
+//             }
+//         }
+        
+//         for(int[] row : grid) {
+//             for(int col : row) {
+//                 if(col == 0) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int grid[][] = new int[m][n], ans = 0;
-        for(int[] wall : walls) {
+        int[][] grid = new int[m][n];
+        int r, c;
+        for (int[] guard : guards) {
+            grid[guard[0]][guard[1]] = 1;
+        }
+        for (int[] wall : walls) {
             grid[wall[0]][wall[1]] = 1;
         }
-        for(int[] guard : guards) 
-            grid[guard[0]][guard[1]] = 2;
-
-        for(int[] guard : guards) {
-            for(int i = guard[1] + 1; i < n; i++) {
-                if(grid[guard[0]][i] == 1 || grid[guard[0]][i] == 2) break;
-                grid[guard[0]][i] = 3;
+        int count = 0;
+        for (int[] guard : guards) {
+            r = guard[0] - 1;
+            c = guard[1];
+            while (r >= 0 && grid[r][c] != 1) {
+                if (grid[r][c] != -1) {
+                    count++;
+                    grid[r][c] = -1;
+                }
+                r--;
             }
-            for(int i = guard[1] - 1; i >= 0; i--) {
-                if(grid[guard[0]][i] == 1 || grid[guard[0]][i] == 2) break;
-                grid[guard[0]][i] = 3;
+            r = guard[0] + 1;
+            while (r < m && grid[r][c] != 1) {
+                if (grid[r][c] != -1) {
+                    count++;
+                    grid[r][c] = -1;
+                }
+                r++;
             }
-            for(int i = guard[0] + 1; i < m; i++) {
-                if(grid[i][guard[1]] == 1 || grid[i][guard[1]] == 2) break;
-                grid[i][guard[1]] = 3;
+            r = guard[0];
+            c = guard[1] - 1;
+            while (c >= 0 && grid[r][c] != 1) {
+                if (grid[r][c] != -1) {
+                    count++;
+                    grid[r][c] = -1;
+                }
+                c--;
             }
-            for(int i = guard[0] - 1; i >= 0; i--) {
-                if(grid[i][guard[1]] == 1 || grid[i][guard[1]] == 2) break;
-                grid[i][guard[1]] = 3;
+            c = guard[1] + 1;
+            while (c < n && grid[r][c] != 1) {
+                if (grid[r][c] != -1) {
+                    count++;
+                    grid[r][c] = -1;
+                }
+                c++;
             }
         }
-        
-        for(int[] row : grid) {
-            for(int col : row) {
-                if(col == 0) ans++;
-            }
-        }
-        return ans;
+        return m * n - guards.length - walls.length - count;
     }
 }
