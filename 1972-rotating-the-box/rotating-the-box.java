53d98e29b77c4ee7b016f57c80e96d2252e60bf4
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-11-23 10:53:11 +0530

    Time: 5 ms (100.00%) | Memory: 78.8 MB (91.92%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -6,7 +6,7 @@ class Solution {
 			for (int j = n - 1, k = n - 1; j >= 0; --j) {
 				res[j][m - i - 1] = '.';
 				if (box[i][j] != '.') {
-					if(box[i][j] == '*') k = j;
+					k = box[i][j] == '*' ? j : k;
 					res[k--][m - i - 1] = box[i][j];
 				}
 			}
