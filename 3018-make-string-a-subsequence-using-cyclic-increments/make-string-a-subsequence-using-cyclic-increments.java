f146aa935768b2de9a3f75c9c45c2eafd4132108
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-12-04 11:11:55 +0530

    Time: 5 ms (100.00%) | Memory: 45.6 MB (53.29%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -3,11 +3,10 @@ class Solution {
         if(str1.length() < str2.length()) return false;
         char[] arr1 = str1.toCharArray(), arr2 = str2.toCharArray();
         int idx = 0;
-        for(char ch : arr1) {
-            if(ch == arr2[idx] || (char)(((ch + 1) > 122) ? 97 : (ch + 1)) == arr2[idx]) 
+        for(char ch : arr1)
+            if(ch == arr2[idx] || (char)((++ch > 122) ? 97 : ch) == arr2[idx]) 
                 if(++idx == arr2.length) return true;
             
-        }
         return false;
     }
 }
