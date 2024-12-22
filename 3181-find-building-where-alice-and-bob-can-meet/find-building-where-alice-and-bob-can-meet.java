07d9ce5924bf31a1f63afe678f635de8835d203c
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-12-22 11:18:30 +0530

    Time: 29 ms (100.00%) | Memory: 73.6 MB (50.00%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.java
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.java
@@ -1,62 +1,40 @@
 class Solution {
     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        int n = heights.length;
-        int[][] st = new int[n][20];
-        int[] Log = new int[n + 1];
-        Log[0] = -1;
-        
-        for (int i = 1; i <= n; i++) {
-            Log[i] = Log[i >> 1] + 1;
-        }
-
-        for (int i = 0; i < n; i++) {
-            st[i][0] = heights[i];
-        }
-
-        for (int i = 1; i < 20; i++) {
-            for (int j = 0; j + (1 << i) <= n; j++) {
-                st[j][i] = Math.max(st[j][i - 1], st[j + (1 << (i - 1))][i - 1]);
-            }
-        }
-
+        List<int[]>[] lists = new ArrayList[heights.length];
         int[] res = new int[queries.length];
-        
+        Arrays.fill(res, -1);
         for (int i = 0; i < queries.length; i++) {
-            int l = queries[i][0], r = queries[i][1];
-            if (l > r) {
-                int temp = l;
-                l = r;
-                r = temp;
-            }
-
-            if (l == r) {
-                res[i] = l;
-                continue;
-            }
-
-            if (heights[r] > heights[l]) {
-                res[i] = r;
-                continue;
-            }
-
-            int maxHeight = Math.max(heights[l], heights[r]);
-            int left = r + 1, right = n, mid;
-
-            while (left < right) {
-                mid = (left + right) / 2;
-                int k = Log[mid - r + 1];
-                int maxInRange = Math.max(st[r][k], st[mid - (1 << k) + 1][k]);
-
-                if (maxInRange > maxHeight) {
-                    right = mid;
+            if (queries[i][0] > queries[i][1]) {
+                if (heights[queries[i][0]] > heights[queries[i][1]]) {
+                    res[i] = queries[i][0];
                 } else {
-                    left = mid + 1;
+                    if (lists[queries[i][0]] == null)
+                        lists[queries[i][0]] = new ArrayList<>();
+                    lists[queries[i][0]].add(new int[] { heights[queries[i][1]], i });
+                }
+            } else if (queries[i][0] < queries[i][1]) {
+                if (heights[queries[i][0]] < heights[queries[i][1]]) {
+                    res[i] = queries[i][1];
+                } else {
+                    if (lists[queries[i][1]] == null)
+                        lists[queries[i][1]] = new ArrayList<>();
+                    lists[queries[i][1]].add(new int[] { heights[queries[i][0]], i });
+                }
+            } else {
+                res[i] = queries[i][0];
+            }
+        }
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < heights.length; i++) {
+            while (!PQ.isEmpty() && PQ.peek()[0] < heights[i]) {
+                res[PQ.poll()[1]] = i;
+            }
+            if (lists[i] != null) {
+                for (int[] arr : lists[i]) {
+                    PQ.add(arr);
                 }
             }
-
-            res[i] = (left == n) ? -1 : left;
         }
-
         return res;
     }
 }
