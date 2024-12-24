7e421e653d5c6b2386c394da060fa74c25e2f78c
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2024-12-24 09:55:07 +0530

    Time: 91 ms (81.89%) | Memory: 114.7 MB (68.50%) - LeetSync

--- 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.java
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.java
@@ -1,67 +1,44 @@
-import java.util.*;
-
 class Solution {
-    int dia;
+    private int dia;
 
     public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
-        int n1 = edges1.length + 1; 
-        int n2 = edges2.length + 1;
-
-        ArrayList<Integer>[] graph1 = new ArrayList[n1];
-        ArrayList<Integer>[] graph2 = new ArrayList[n2];
-
-        for (int i = 0; i < n1; i++) graph1[i] = new ArrayList<>();
-        for (int i = 0; i < n2; i++) graph2[i] = new ArrayList<>();
-
-        for (int i = 0; i < edges1.length; i++) {
-            int u = edges1[i][0];
-            int v = edges1[i][1];
-            graph1[u].add(v);
-            graph1[v].add(u);
+        int n = edges1.length + 1, m = edges2.length + 1;
+        List<Integer>[] t1 = new List[n], t2 = new List[m];
+        Arrays.setAll(t1, i -> new ArrayList<>());
+        Arrays.setAll(t2, i -> new ArrayList<>());
+        for (int[] e : edges1) {
+            int u = e[0], v = e[1];
+            t1[u].add(v);
+            t1[v].add(u);
         }
-
-        for (int i = 0; i < edges2.length; i++) {
-            int u = edges2[i][0];
-            int v = edges2[i][1];
-            graph2[u].add(v);
-            graph2[v].add(u);
+        for (int[] e : edges2) {
+            int u = e[0], v = e[1];
+            t2[u].add(v);
+            t2[v].add(u);
         }
-
-        dia = -1;
-        boolean[] visited1 = new boolean[n1]; 
-        getDia(0, graph1, visited1);
-        int d1 = dia;
-
-        dia = -1;
-        boolean[] visited2 = new boolean[n2]; 
-        getDia(0, graph2, visited2);
-        int d2 = dia;
-
-        int cand = (d1 + 1) / 2 + (d2 + 1) / 2 + 1;
-
-       
-        return Math.max(cand, Math.max(d1, d2));
+        dia = 0;
+        diameterOf(t1, 0, -1);
+        int dia1 = dia;
+        dia = 0;
+        diameterOf(t2, 0, -1);
+        int dia2 = dia;
+        return Math.max(Math.max(dia1, dia2), (dia1 + 1) / 2 + 1 + (dia2 + 1) / 2);
     }
 
-    private int getDia(int src, ArrayList<Integer>[] graph, boolean[] visited) {
-        visited[src] = true;
-        int dch = -1;
-        int sdch = -1;
-
-        for (int child : graph[src]) {
-            if (!visited[child]) {
-                int ch = getDia(child, graph, visited);
-                if (ch > dch) {
-                    sdch = dch;
-                    dch = ch;
-                } else if (ch > sdch) {
-                    sdch = ch;
-                }
+    private int diameterOf(List<Integer>[] tree, int i, int pa) {
+        int max1 = 0, max2 = 0;
+        for (int j : tree[i]) {
+            if (j == pa) continue;
+            int d = diameterOf(tree, j, i);
+            if (max1 <= d) {
+                max2 = max1;
+                max1 = d;
+            } else if (max2 < d) {
+                max2 = d;
             }
         }
-
-        if (dch + sdch + 2 > dia) dia = dch + sdch + 2;
-        dch += 1;
-        return dch;
+        dia = Math.max(dia, max1 + max2);
+        return max1 + 1;
     }
+
 }
