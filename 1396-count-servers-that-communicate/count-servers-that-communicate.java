c65b5a64d19dd4920ee2236bbcc61d8bdc2a5e89
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2025-01-24 10:31:06 +0530

    Time: 2 ms (100.00%) | Memory: 52.4 MB (70.03%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countServers(int[][] grid) {
+        int rows = grid.length, cols = rows == 0 ? 0 : grid[0].length, servers = 0;
+        int[] countServersInRow = new int[rows], countServersInCol = new int[cols];
+        
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == 1) {
+                    ++countServersInRow[row];
+                    ++countServersInCol[col];
+                }
+            }
+        }
+        
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+                if (grid[row][col] == 1 && (countServersInRow[row] > 1 || countServersInCol[col] > 1))
+                    ++servers;
+        
+        return servers;
+    }
+}
