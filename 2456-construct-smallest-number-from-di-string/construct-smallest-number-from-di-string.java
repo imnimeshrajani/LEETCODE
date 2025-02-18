6fad24717ee93d29d2bf488c9990ff9b4cc65927
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2025-02-18 22:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (36.89%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -20,10 +20,8 @@ class Solution {
     public void reverse(char arr[] , int i  , int j){
             while(i < j){
                 char temp = arr[i];
-                arr[i]= arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
+                arr[i++]= arr[j];
+                arr[j--] = temp;
             }  
 
     }
