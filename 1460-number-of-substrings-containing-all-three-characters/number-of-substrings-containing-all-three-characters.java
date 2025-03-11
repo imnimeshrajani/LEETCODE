ca72d2e1a00aa9461cf063ac4ece0e5d254e785f
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2025-03-11 09:45:33 +0530

    Time: 4 ms (100.00%) | Memory: 44.6 MB (64.19%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -3,16 +3,13 @@ class Solution {
         char[] ch = s.toCharArray();
         int[] abc = new int[] {-1, -1, -1};
         int count = 0, right = 0;
-        while(right < ch.length)
-        {
-            abc[ch[right] - 'a'] = right;
+        while(right < ch.length) {
+            abc[ch[right] - 'a'] = right++;
             int min = Integer.MAX_VALUE;
             for(int i=0; i<3; i++)
-            {
                 min = Math.min(min, abc[i]);
-            }
+            
             count += (min+1);
-            right++;
         }
         return count;
     }
