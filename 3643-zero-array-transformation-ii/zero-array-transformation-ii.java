7cc795f449bf31c987a9d6e0bb6c7d1d5387c10a
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2025-03-13 14:17:11 +0530

    Time: 3 ms (100.00%) | Memory: 129.1 MB (16.15%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,35 +1,24 @@
 class Solution {
-    private boolean isZeroArray(int[] nums, int[][] queries, int mid) {
-        int n = nums.length;
-        int[] temp = nums.clone(), delta = new int[n + 1];
-
-        for (int i = 0; i < mid; i++) {
-            int l = queries[i][0], r = queries[i][1], v = queries[i][2];
-            delta[l] -= v;
-            if (r + 1 < n) delta[r + 1] += v;
-        }
-
-        int currDecrement = 0;
-        for (int i = 0; i < n; i++) {
-            currDecrement += delta[i];
-            temp[i] += currDecrement;
-            if (temp[i] > 0) return false;
-        }
-        return true;
-    }
-
     public int minZeroArray(int[] nums, int[][] queries) {
-        int left = 0, right = queries.length, ans = -1;
+        int n = nums.length, sum = 0, k = 0;  
+        int[] differenceArray = new int[n + 1];  
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isZeroArray(nums, queries, mid)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        for (int index = 0; index < n; index++) {
+            while (sum + differenceArray[index] < nums[index]) {  
+                k++;
+                if (k > queries.length) {  
+                    return -1;
+                }
+
+                int left = queries[k - 1][0], right = queries[k - 1][1], val =  queries[k - 1][2];  
+
+                if (right >= index) {  
+                    differenceArray[Math.max(left, index)] += val;  
+                    differenceArray[right + 1] -= val; 
+                }
             }
+            sum += differenceArray[index];  
         }
-        return ans;
+        return k; 
     }
 }
