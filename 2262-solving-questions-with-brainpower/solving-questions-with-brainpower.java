3c7a1ff9c27f7589deebaf82a96d466ebf5037ed
Nimesh Rajani <104212824+imnimeshrajani@users.noreply.github.com>
2025-04-01 09:34:49 +0530

    Time: 1 ms (100.00%) | Memory: 108.5 MB (53.39%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,13 +1,47 @@
-class Solution {
-    public long mostPoints(int[][] questions) {
-        long[] dp = new long[questions.length];
-        return helper(questions, dp, 0);
-    }
+// class Solution {
+//     public long mostPoints(int[][] questions) {
+//         long[] dp = new long[questions.length];
+//         return helper(questions, dp, 0);
+//     }
 
-    private long helper(int[][] que, long[] dp, int idx) {
-        if(idx >=que.length) return 0;
-        if(dp[idx] != 0) return dp[idx];
-        int ans = que[idx][0], nextIdx = idx + que[idx][1] + 1;
-        return dp[idx] = Math.max(helper(que, dp, idx + 1), que[idx][0] + helper(que, dp, nextIdx));
-    } 
+//     private long helper(int[][] que, long[] dp, int idx) {
+//         if(idx >=que.length) return 0;
+//         if(dp[idx] != 0) return dp[idx];
+//         int ans = que[idx][0], nextIdx = idx + que[idx][1] + 1;
+//         return dp[idx] = Math.max(helper(que, dp, idx + 1), que[idx][0] + helper(que, dp, nextIdx));
+//     } 
+// }
+
+
+class Solution {
+    private long calcMaxPoints(int[][] questions, int index, int n, long[] dp) {
+        if (index == n) return 0;
+        if (dp[index] != -1) return dp[index];
+        long take = questions[index][0];
+        int newIndex = index + questions[index][1] + 1;
+        if (newIndex < n) take += calcMaxPoints(questions, index + questions[index][1] + 1, n, dp);
+        long notTake = calcMaxPoints(questions, index + 1, n, dp);
+
+        return dp[index] = Math.max(take, notTake);
+    }
+    static {
+        int[][] arr = {{3,2},{4,3},{4,4},{2,5}};
+        for (int i = 0 ; i < 100 ; i++) mostPoints(arr);
+    }
+    public static long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];
+        // Arrays.fill(dp, -1);
+        // return calcMaxPoints(questions, 0, n, dp);
+        dp[n - 1] = questions[n - 1][0];
+        for (int i = n - 2 ; i >= 0 ; i--) {
+            long take = questions[i][0];
+            int newIndex = i + questions[i][1] + 1;
+            take += (newIndex < n) ? dp[newIndex] : 0;
+            long notTake = dp[i + 1];
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[0];
+    }
 }
